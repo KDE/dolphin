aee654cfae3c431508246b3ef8ad774df46e2153
Simon Hausmann <hausmann@kde.org>
1999-03-22 19:46:11 +0000

    ooooops, I forgot
    svn path=/trunk/kdebase/konqueror/; revision=18444

---
+++ konqueror/konq_plugins.cc
@@ -0,0 +1,202 @@
+
+#include "konq_plugins.h"
+
+#include <opApplication.h>
+#include <opIMR.h>
+
+#include <kservices.h>
+#include <ksimpleconfig.h>
+
+QDict< QList<KonqPlugins::imrActivationEntry> > KonqPlugins::s_dctViewServers;
+QDict< QList<KonqPlugins::imrActivationEntry> > KonqPlugins::s_dctPartServers;
+QDict< QList<KonqPlugins::imrActivationEntry> > KonqPlugins::s_dctEventFilterServers;
+
+void KonqPlugins::init()
+{
+  imr_init();
+  
+  s_dctViewServers.setAutoDelete( true );
+  s_dctPartServers.setAutoDelete( true );
+  s_dctEventFilterServers.setAutoDelete( true );
+}
+
+bool KonqPlugins::isPluginServiceType( const QString serviceType, bool *isView, bool *isPart, bool *isEventFilter )
+{
+  list<KService::Offer> lstOffers;
+  bool b1, b2, b3;
+
+  b1 = ( s_dctViewServers[ serviceType ] != 0L );
+  b2 = ( s_dctPartServers[ serviceType ] != 0L );
+  b3 = ( s_dctEventFilterServers[ serviceType ] != 0L );
+  
+  if ( isView )
+    *isView = b1;
+    
+  if ( isPart )
+    *isPart = b2;
+    
+  if ( isEventFilter )
+    *isEventFilter = b3;
+
+  if ( b1 || b2 || b3 )
+    return true;
+  
+  KService::findServiceByServiceType( serviceType, lstOffers );
+  
+  if ( lstOffers.size() == 0 )
+    return false;
+
+  bool res = false;
+
+  list<KService::Offer>::iterator it = lstOffers.begin();
+  for (; it != lstOffers.end(); ++it )
+  {
+    if ( it->m_pService->file() )
+    {
+      parseService( it->m_pService->file(), serviceType, &b1, &b2, &b3 );
+
+      if ( b1 )
+      {
+        res = true;
+	if ( isView )
+	  *isView = true;
+      }
+      
+      if ( b2 )
+      {
+        res = true;
+	if ( isPart )
+	  *isPart = true;
+      }
+      
+      if ( b3 )
+      {
+        res = true;
+	if ( isEventFilter )
+	  *isEventFilter = true;
+      }
+      
+    }
+          
+  }
+
+  return res;    
+}
+
+CORBA::Object_ptr KonqPlugins::lookupServer( const QString serviceType, ServerType sType )
+{
+  QList<imrActivationEntry> *l = 0L;
+  
+  switch ( sType )
+  {
+    case View: l = s_dctViewServers[ serviceType ]; break;
+    case Part: l = s_dctPartServers[ serviceType ]; break;
+    case EventFilter: l = s_dctEventFilterServers[ serviceType ]; break;
+  }
+  
+  if ( !l )
+    return CORBA::Object::_nil();
+    
+  imrActivationEntry *e = l->getFirst(); //hm.....
+  
+  if ( !e )
+    return CORBA::Object::_nil();
+    
+  return imr_activate( e->serverName, e->repoID );    
+}
+ 
+void KonqPlugins::parseService( const QString file, const QString serviceType, bool *isView, bool *isPart, bool *isEventFilter )
+{
+  KSimpleConfig config( file, true );
+  QString serverExec;
+  QString serverName;
+  QString activationMode;
+  QStrList repoIDs;
+
+  *isView = false;
+  *isPart = false;
+  *isEventFilter = false;
+  
+  config.setGroup( "KDE Desktop Entry" );
+  
+  QStrList serviceTypes;
+  config.readListEntry( "ServiceTypes", serviceTypes );
+  if ( serviceTypes.count() == 0 )
+    return;
+
+  serverExec = config.readEntry( "ServerExec" );
+  if ( serverExec.isNull() )
+    return;
+    
+  serverName = config.readEntry( "ServerName" );
+  if ( serverName.isNull() )
+    return;     
+
+  activationMode = config.readEntry( "ActivationMode" );
+  if ( activationMode.isNull() )
+    return;
+    
+  repoIDs.setAutoDelete( true );        
+    
+  QStrListIterator it( serviceTypes );
+  for (; it.current(); ++it )
+  {
+    QList<imrActivationEntry> *l = 0L;
+    
+    config.setGroup( QString("ServiceType ") + it.current() );
+    
+    if ( strcmp( it.current(), "KonquerorView" ) == 0 )
+    {
+      *isView = true;
+      if ( !( l = s_dctViewServers[ serviceType ] ) )
+      {
+        l = new QList<imrActivationEntry>;
+	l->setAutoDelete( true );
+	s_dctViewServers.insert( serviceType, l );
+      }
+    }
+    else if ( strcmp( it.current(), "OpenPart" ) == 0 )
+    {
+      *isPart = true;
+      if ( !( l = s_dctPartServers[ serviceType ] ) )
+      {
+        l = new QList<imrActivationEntry>;
+	l->setAutoDelete( true );
+	s_dctPartServers.insert( serviceType, l );
+      }
+    }
+    else if ( strcmp( it.current(), "KonquerorEventFilter" ) == 0 )
+    {
+      *isEventFilter = true;
+      if ( !( l = s_dctEventFilterServers[ serviceType ] ) )
+      {
+        l = new QList<imrActivationEntry>;
+	l->setAutoDelete( true );
+	s_dctEventFilterServers.insert( serviceType, l );
+      }
+    }
+    
+    if ( l )
+    {
+      imrActivationEntry *e = new imrActivationEntry;
+      e->serverName = serverName;
+      e->repoID = config.readEntry( "RepoID" );
+      if ( !e->repoID.isNull() )
+      {
+        l->append( e );
+	repoIDs.append( e->repoID );
+      }
+    }
+    else
+    {
+      cerr << "unknown servicetype!!!!!!!!!!!!!!" << endl;
+      return;
+    }
+    
+  }
+
+  CORBA::Object_var obj = opapp_orb->resolve_initial_references( "ImplementationRepository" );
+  CORBA::ImplRepository_var imr = CORBA::ImplRepository::_narrow( obj );
+  assert( !CORBA::is_nil( imr ) );
+  imr_create( serverName, activationMode, serverExec, repoIDs, imr );
+}
---
+++ konqueror/konq_plugins.h
@@ -0,0 +1,35 @@
+#ifndef __konq_plugins_h__
+#define __konq_plugins_h__
+
+#include <CORBA.h>
+
+#include <qstring.h>
+#include <qdict.h>
+#include <qlist.h>
+
+class KonqPlugins
+{
+public:
+  enum ServerType { View, Part, EventFilter };
+
+  static void init();
+   
+  static bool isPluginServiceType( const QString serviceType, bool *isView = 0L, bool *isPart = 0L, bool *isEventFilter = 0L );
+  static CORBA::Object_ptr lookupServer( const QString serviceType, ServerType sType );
+  
+private:
+  static void parseService( const QString file, const QString serviceType, bool *isView, bool *isPart, bool *isEventFilter );
+
+  struct imrActivationEntry
+  {
+    QString serverName;
+    QString repoID;
+  };
+  
+  static QDict< QList<imrActivationEntry> > s_dctViewServers;
+  static QDict< QList<imrActivationEntry> > s_dctPartServers;
+  static QDict< QList<imrActivationEntry> > s_dctEventFilterServers;
+  static QList<imrActivationEntry> s_lstMainViewKOMPlugins;
+};
+
+#endif
