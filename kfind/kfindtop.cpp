4b64f95da899637fc38187c1ea056eb816f14c8e
Cristian Tibirna <tibirna@kde.org>
1999-10-22 03:45:01 +0000

    CT: all compiles apart kvt (and kikbd on my disk): those API shifts
    svn path=/trunk/kdebase/kfind/; revision=31477

--- kfind/kfindtop.cpp
+++ kfind/kfindtop.cpp
@@ -156,7 +156,7 @@ void KfindTop::menuInit()
   
   QString tmp = i18n("KFind %1\nFrontend to find utility\nMiroslav Flídr <flidr@kky.zcu.cz>\n\nSpecial thanks to Stephan Kulow\n<coolo@kde.org>")
     .arg(KFIND_VERSION);
-  _helpMenu=kapp->helpMenu( true, tmp );    
+  _helpMenu=helpMenu( tmp );    
   
   _mainMenu = new KMenuBar(this, "_mainMenu");
   _mainMenu->insertItem( i18n("&File"), _fileMenu);
