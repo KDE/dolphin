a42cbca9002f77b55831dd4e0f19a25dfc5991d9
David Faure <faure@kde.org>
1999-11-13 13:23:44 +0000

    Convert user's bookmarks to .desktop and to remove .xpm from icons
    svn path=/trunk/kdebase/konqueror/; revision=33672

--- konqueror/convert_kdelnk.sh
+++ konqueror/convert_kdelnk.sh
@@ -10,3 +10,18 @@ find ~/.kde/share/mimelnk ~/.kde/share/applnk -name "*.kdelnk" -print |
   mv "$k" "$d"
  done
 
+# Convert user's bookmarks to .desktop and to remove .xpm from icons
+find ~/.kde/share/apps/kfm/bookmarks -type f -print |
+ while read k; do
+  if echo $k | grep -q kdelnk; then  # kdelnk file
+    d=`echo $k|sed -e 's/\.kdelnk/\.desktop/'`
+    echo "Renaming $k to $d"
+  else
+    d=$k
+    k=$d".tmp"
+    mv "$d" "$k"
+  fi
+  sed -e 's/\.xpm//' "$k" > "$d"
+  rm -f "$k"
+ done
+
