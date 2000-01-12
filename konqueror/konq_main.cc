fbc552f798d43edfc0476a9de303daaa79f3bb50
Waldo Bastian <bastian@kde.org>
2000-01-12 17:20:20 +0000

    WABA: "delete args;" --> "args->clear();"
    svn path=/trunk/kdebase/konqueror/; revision=38166

--- konqueror/konq_main.cc
+++ konqueror/konq_main.cc
@@ -155,7 +155,7 @@ int main( int argc, char **argv )
      for ( int i = 0; i < args->count(); i++ )
         fm.openFileManagerWindow( args->arg(i) );
   }
-  delete args;
+  args->clear();
 
   QObject::connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
 
