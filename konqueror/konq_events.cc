932c448abe0961ceb34a74a141c1a281d346d0e0
Simon Hausmann <hausmann@kde.org>
2000-01-19 19:56:43 +0000

    - use the KParts::Event stuff
    svn path=/trunk/kdebase/konqueror/; revision=38773

--- konqueror/konq_events.cc
+++ konqueror/konq_events.cc
@@ -2,12 +2,3 @@
 #include "konq_events.h"
 
 const char *KonqURLEnterEvent::s_urlEnterEventName = "Konqueror/URLEntered";
-
-bool KonqURLEnterEvent::test( QEvent *e )
-{
-  if ( e->type() == ( QEvent::User + KONQ_EVENT_MAGIC ) &&
-       strcmp( ((KonqEvent *)e)->eventName(), s_urlEnterEventName ) == 0 )
-    return true;
-
-  return false;
-}
--- konqueror/konq_events.h
+++ konqueror/konq_events.h
@@ -1,30 +1,16 @@
 #ifndef __konq_events_h__
 #define __konq_events_h__ $Id$
 
-#include <qevent.h>
+#include <kevent.h>
 
-#define KONQ_EVENT_MAGIC 2437
-
-class KonqEvent : public QCustomEvent
+class KonqURLEnterEvent : public KParts::Event
 {
 public:
-  KonqEvent() : QCustomEvent( (QEvent::Type)(QEvent::User + KONQ_EVENT_MAGIC), (void *)0 ) {};
-  virtual ~KonqEvent() {};
-
-  virtual const char *eventName() const = 0;
-};
-
-class KonqURLEnterEvent : public KonqEvent
-{
-public:
-  KonqURLEnterEvent( const QString &url ) : m_url( url ) {};
-  virtual ~KonqURLEnterEvent() {};
+  KonqURLEnterEvent( const QString &url ) : KParts::Event( s_urlEnterEventName ), m_url( url ) {};
 
   QString url() const { return m_url; }
 
-  virtual const char *eventName() const { return s_urlEnterEventName; }
-
-  static bool test( QEvent *event );
+  static bool test( const QEvent *event ) { return KParts::Event::test( event, s_urlEnterEventName ); }
 
 private:
   static const char *s_urlEnterEventName;
