0bcac7f2711ea4f419cb19ea6e1e27b58be8c10b
Stefan Schimanski <schimmi@kde.org>
2000-04-06 00:29:25 +0000

    Added some more testing code.
    svn path=/trunk/kdebase/nsplugins/; revision=45742

--- nsplugins/main.cpp
+++ nsplugins/main.cpp
@@ -1,6 +1,6 @@
+#include <stdio.h>
+
 #include <qstring.h>
-
-
 #include <kapp.h>
 #include <kcmdlineargs.h>
 #include <dcopclient.h>
@@ -14,11 +14,13 @@
 
 int main(int argc, char *argv[])
 {
+  setvbuf( stderr, NULL, _IONBF, 0 );
   KCmdLineArgs::init(argc, argv, "nsplugin", "A Netscape Plugin test program", "0.1");
 
   KApplication app(argc, argv, "nsplugin");
 
   app.dcopClient()->attach();
+  app.dcopClient()->registerAs(app.name());
   app.dcopClient()->setNotifications(true);
 
 
@@ -26,10 +28,20 @@ int main(int argc, char *argv[])
   
   QWidget *win;
 
+  //QString src = "file:/home/sschimanski/kimble_themovie.swf";
+  QString src = "file:/home/sschimanski/autsch.swf";
+  QString mime = "application/x-shockwave-flash";
+
+  //QString src = "file:/opt/kde/share/Circuit.jpg";
+  //QString mime = "image/jpg";
+
+  //QString src = "file:/home/sschimanski/hund.avi";
+  //QString mime = "video/avi";
+
   QStringList _argn, _argv;
   _argn << "SRC" << "TYPE" << "WIDTH" << "HEIGHT";
-  _argv << "file:/home/sschimanski/autsch.swf" << "application/x-shockwave-flash" << "400" << "250";
-  win = loader->NewInstance(0, "file:/home/sschimanski/autsch.swf", "application/x-shockwave-flash", 1, _argn, _argv);
+  _argv << src << mime << "400" << "250";
+  win = loader->NewInstance(0, src, mime, 1, _argn, _argv);
   
   if (win)
     {
@@ -38,5 +50,4 @@ int main(int argc, char *argv[])
     }
   
   app.exec();
-
 }
