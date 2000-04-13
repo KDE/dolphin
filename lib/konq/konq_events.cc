cd6902c50b14b8b718696e1ad6f56ec227a04140
nobody <nobody@localhost>
2000-04-13 20:33:21 +0000

    This commit was manufactured by cvs2svn to accommodate a server-side copy/move.
    svn path=/trunk/kdebase/libkonq/; revision=46538

---
+++ lib/konq/konq_events.cc
@@ -0,0 +1,6 @@
+
+#include "konq_events.h"
+
+const char *KonqURLEnterEvent::s_urlEnterEventName = "Konqueror/URLEntered";
+
+const char *KonqFileSelectionEvent::s_fileItemSelectionEventName = "Konqueror/FileSelection";
