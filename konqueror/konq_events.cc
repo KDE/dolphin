e6eb69ed69218e7a8b8d730d5d0f380c0d24ab2a
David Faure <faure@kde.org>
2000-04-13 20:42:45 +0000

    Moved to libkonq
    svn path=/trunk/kdebase/konqueror/; revision=46540

--- konqueror/konq_events.cc
+++
@@ -1,6 +0,0 @@
-
-#include "konq_events.h"
-
-const char *KonqURLEnterEvent::s_urlEnterEventName = "Konqueror/URLEntered";
-
-const char *KonqFileSelectionEvent::s_fileItemSelectionEventName = "Konqueror/FileSelection";
