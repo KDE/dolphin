e61b3e2aa172330f308049ec9ee041eed547e28b
Stefan Schimanski <schimmi@kde.org>
2000-04-14 21:56:13 +0000

    Added class/instance deletion on app shutdown.
    svn path=/trunk/kdebase/nsplugins/; revision=46625

--- nsplugins/main.cpp
+++ nsplugins/main.cpp
@@ -26,10 +26,8 @@ int main(int argc, char *argv[])
 
   NSPluginLoader *loader = NSPluginLoader::instance();
   
-  QWidget *win;
-
   //QString src = "file:/home/sschimanski/kimble_themovie.swf";
-  QString src = "file:/home/lstcore/mhk/autsch.swf";
+  QString src = "file:/home/sschimanski/autsch.swf";
   QString mime = "application/x-shockwave-flash";
 
   //QString src = "file:/opt/kde/share/Circuit.jpg";
@@ -41,12 +39,12 @@ int main(int argc, char *argv[])
   QStringList _argn, _argv;
   _argn << "SRC" << "TYPE" << "WIDTH" << "HEIGHT";
   _argv << src << mime << "400" << "250";
-  win = loader->NewInstance(0, src, mime, 1, _argn, _argv);
+  QWidget *win = loader->NewInstance(0, src, mime, 1, _argn, _argv);
 /*  
   QStringList _argn, _argv;
   _argn << "TYPE" << "WIDTH" << "HEIGHT" << "java_docbase" << "CODE";
   _argv << "application/x-java-applet" << "450" << "350" << "file:///none" << "sun/plugin/panel/ControlPanelApplet.class";
-  win = loader->NewInstance(0, "", "application/x-java-applet", 1, _argn, _argv);
+  QWidget *win = loader->NewInstance(0, "", "application/x-java-applet", 1, _argn, _argv);
  */
 
   if (win)
@@ -55,5 +53,8 @@ int main(int argc, char *argv[])
       win->show();
   
       app.exec();
+
+      delete win;
+      delete loader;
     }
 }
