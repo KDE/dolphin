94342275178713561e189410b28f7277863d0920
David Faure <faure@kde.org>
2000-06-09 15:02:51 +0000

    Implemented network-transparent image preview. This hasn't been easy, but it's a very good testbed for KIO::NetAccess's API. I can't test the change (over 8 bit VNC), but I can count on Ferdinand for that :)
    svn path=/trunk/kdebase/libkonq/; revision=52733

--- lib/konq/konqfileitem.cc
+++ lib/konq/konqfileitem.cc
@@ -19,6 +19,9 @@
 // $Id$
 
 #include <sys/time.h>
+#include <unistd.h>
+#include <kio/netaccess.h>
+#include <ktempfile.h>
 
 #include "konqfileitem.h"
 
@@ -33,7 +36,7 @@ QPixmap KonqFileItem::pixmap( int _size, int _state,
 	                      bool bImagePreviewAllowed ) const
 {
   if ( m_pMimeType && m_pMimeType->name().left(6) == "image/" &&
-       m_bIsLocalURL && bImagePreviewAllowed )
+       bImagePreviewAllowed )
   {
       kdDebug(1203) << "Requested thumbnail size: " << _size << endl;
       if(_size == 0) // default size requested
@@ -41,156 +44,198 @@ QPixmap KonqFileItem::pixmap( int _size, int _state,
           KIconTheme *root = KGlobal::instance()->iconLoader()->theme();
           _size = root->defaultSize( KIcon::Desktop );
       }
-      struct stat buff;
       bool bAvail = false;
-      QString thumbPath;
+      KURL thumbURL( m_url );
       QPixmap pix;
+      KIO::UDSEntry entry;
 
       // Check if pixie thumbnail of any size is there first, preferring
       // the pixie equivalent to the requested icon size
       if(_size < 28){
-          thumbPath = m_url.directory() + "/.mospics/small/" + m_url.fileName();
-          bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+          thumbURL.setPath( m_url.directory() + "/.mospics/small/" + m_url.fileName() );
+          bAvail = KIO::NetAccess::stat(thumbURL, entry);
           if(!bAvail){
-              thumbPath = m_url.directory() + "/.mospics/med/" + m_url.fileName();
-              bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+              thumbURL.setPath( m_url.directory() + "/.mospics/med/" + m_url.fileName() );
+              bAvail = KIO::NetAccess::stat(thumbURL, entry);
           }
           if(!bAvail){
-              thumbPath = m_url.directory() + "/.mospics/large/" + m_url.fileName();
-              bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+              thumbURL.setPath( m_url.directory() + "/.mospics/large/" + m_url.fileName() );
+              bAvail = KIO::NetAccess::stat(thumbURL, entry);
           }
       }
       else if(_size < 40){
-          thumbPath = m_url.directory() + "/.mospics/med/" + m_url.fileName();
-          bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+          thumbURL.setPath( m_url.directory() + "/.mospics/med/" + m_url.fileName() );
+          bAvail = KIO::NetAccess::stat(thumbURL, entry);
           if(!bAvail){
-              thumbPath = m_url.directory() + "/.mospics/small/" + m_url.fileName();
-              bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+              thumbURL.setPath( m_url.directory() + "/.mospics/small/" + m_url.fileName() );
+              bAvail = KIO::NetAccess::stat(thumbURL, entry);
           }
           if(!bAvail){
-              thumbPath = m_url.directory() + "/.mospics/large/" + m_url.fileName();
-              bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+              thumbURL.setPath( m_url.directory() + "/.mospics/large/" + m_url.fileName() );
+              bAvail = KIO::NetAccess::stat(thumbURL, entry);
           }
       }
       else{
-          thumbPath = m_url.directory() + "/.mospics/large/" + m_url.fileName();
-          bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+          thumbURL.setPath( m_url.directory() + "/.mospics/large/" + m_url.fileName() );
+          bAvail = KIO::NetAccess::stat(thumbURL, entry);
           if(!bAvail){
-              thumbPath = m_url.directory() + "/.mospics/med/" + m_url.fileName();
-              bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+              thumbURL.setPath( m_url.directory() + "/.mospics/med/" + m_url.fileName() );
+              bAvail = KIO::NetAccess::stat(thumbURL, entry);
           }
           if(!bAvail){
-              thumbPath = m_url.directory() + "/.mospics/small/" + m_url.fileName();
-              bAvail = stat(thumbPath.local8Bit(), &buff) == 0;
+              thumbURL.setPath( m_url.directory() + "/.mospics/small/" + m_url.fileName() );
+              bAvail = KIO::NetAccess::stat(thumbURL, entry);
           }
       }
 
+      // Get time of the orig file
+      time_t tOrig = 0L;
+      if ( KIO::NetAccess::stat( m_url.path(), entry ) ){
+        KIO::UDSEntry::ConstIterator it = entry.begin();
+        for( ; it != entry.end(); it++ ) {
+          if ( (*it).m_uds == KIO::UDS_MODIFICATION_TIME ) {
+            tOrig = (time_t)((*it).m_long);
+          }
+        }
+      } // else... well we have a problem, Houston: no more orig file
+        
       if(bAvail){
-          time_t t1 = buff.st_mtime;
-          // Get time of the orig file
-          if ( lstat( m_url.path().local8Bit(), &buff ) == 0 ){
-              time_t t2 = buff.st_mtime;
-              // Is it outdated ?
-              if ( t1 < t2 )
-                  bAvail = false;
+
+          // Get time of thumbnail file
+          time_t t1 = 0L;
+          KIO::UDSEntry::ConstIterator it = entry.begin();
+          for( ; it != entry.end(); it++ ) {
+            if ( (*it).m_uds == KIO::UDS_MODIFICATION_TIME ) {
+              t1 = (time_t)((*it).m_long);
+            }
           }
-          if(bAvail){
-              if (pix.load(thumbPath))
-                  return pix;
-              else
-                  bAvail = false;
-          }
+
+          // Is the thumbnail outdated ?
+          if ( t1 < tOrig )
+                bAvail = false;
       }
 
-      // check to see if there is an existing Xv thumbnail
+      if (!bAvail) {
+        // check to see if there is an existing Xv thumbnail
 
-      thumbPath = m_url.directory() +
-          ((KURL(m_url.directory()).fileName(true) != ".xvpics") ? "/.xvpics/" : "/");
-      thumbPath += m_url.fileName();
+        thumbURL.setPath( m_url.directory() +    // base dir
+           ((KURL(m_url.directory()).fileName(true) != ".xvpics")
+               ? "/.xvpics/" : "/")              // .xvpics if not already there
+           + m_url.fileName() );                 // file name
 
-      // Is the xv pic available ?
-      if ( stat( thumbPath.local8Bit(), &buff ) == 0 )
-      { // Yes
+        // Is the xv pic available ?
+        if ( KIO::NetAccess::stat( thumbURL, entry ) )
+        {
           bAvail = true;
-          // Get the time of the xv pic
-          time_t t1 = buff.st_mtime;
-          // Get time of the orig file
-          if ( lstat( m_url.path().local8Bit(), &buff ) == 0 )
-          {
-              time_t t2 = buff.st_mtime;
-              // Is it outdated ?
-              if ( t1 < t2 )
-                  bAvail = false;
+          // Get time of thumbnail file
+          time_t t1 = 0L;
+          KIO::UDSEntry::ConstIterator it = entry.begin();
+          for( ; it != entry.end(); it++ ) {
+            if ( (*it).m_uds == KIO::UDS_MODIFICATION_TIME ) {
+              t1 = (time_t)((*it).m_long);
+            }
           }
+
+          // Is the thumbnail outdated ?
+          if ( t1 < tOrig )
+            bAvail = false;
+        }
       }
 
-      if ( bAvail )
-      {
-          if (pix.load( thumbPath ))
+      if(bAvail) {
+          // Load thumbnail
+          QString tmpFile;
+          if ( KIO::NetAccess::download( thumbURL, tmpFile )
+               && pix.load(tmpFile) )
+          {
+              KIO::NetAccess::removeTempFile( tmpFile );
               return pix;
+          }
+          else
+              bAvail = false;
+      }
+
+      // No thumbnail, or too old -> load the orig image and create Pixie pic
+      int extent;
+      QString sizeStr;
+      if(_size < 28){
+          thumbURL.setPath( m_url.directory() + "/.mospics/small/" + m_url.fileName() );
+          extent = 48;
+          sizeStr = "/.mospics/small";
+      }
+      else if(_size < 40){
+          thumbURL.setPath( m_url.directory() + "/.mospics/med/" + m_url.fileName() );
+          extent = 64;
+          sizeStr = "/.mospics/med";
       }
       else{
-          // No xv pic, or too old -> load the orig image and create Pixie pic
-          int extent;
-          QString sizeStr;
-          if(_size < 28){
-              thumbPath = m_url.directory() + "/.mospics/small/" + m_url.fileName();
-              extent = 48;
-              sizeStr = ".mospics/small";
-          }
-          else if(_size < 40){
-              thumbPath = m_url.directory() + "/.mospics/med/" + m_url.fileName();
-              extent = 64;
-              sizeStr = ".mospics/med";
-          }
-          else{
-              thumbPath = m_url.directory() + "/.mospics/large/" + m_url.fileName();
-              extent = 90;
-              sizeStr = ".mospics/large";
-          }
-          if (pix.load(m_url.path()))
-          {
-              bool bCanSave = true;
-              // Create .mospics/large if it doesn't exist
-              QDir mosDir(m_url.directory());
-              if (!mosDir.exists(".mospics"))
-              {
-                  bCanSave = mosDir.mkdir(".mospics");
-              }
-              if (!mosDir.exists(sizeStr))
-              {
-                  bCanSave = mosDir.mkdir(sizeStr);
-              }
-              int w = pix.width(), h = pix.height();
-              // scale to pixie size
-              QImage img;
-              if(pix.width() > extent || pix.height() > extent){
-                  if(pix.width() > pix.height()){
-                      float percent = (((float)extent)/pix.width());
-                      h = (int)(pix.height()*percent);
-                      w = extent;
-                  }
-                  else{
-                      float percent = (((float)extent)/pix.height());
-                      w = (int)(pix.width()*percent);
-                      h = extent;
-                  }
-                  img = pix.convertToImage().smoothScale( w, h );
-                  pix.convertFromImage( img );
-              }
-              else if(bCanSave)
-                  img = pix.convertToImage();
+          thumbURL.setPath( m_url.directory() + "/.mospics/large/" + m_url.fileName() );
+          extent = 90;
+          sizeStr = "/.mospics/large";
+      }
 
-              if (bCanSave)
-              {
-                  // write
-                  QImageIO iio;
-                  iio.setImage(img);
-                  iio.setFileName( thumbPath );
-                  iio.setFormat("PNG");
-                  bCanSave = iio.write();
-                  return pix;
+      QString tmpFile;
+      if ( KIO::NetAccess::download( m_url, tmpFile )
+               && pix.load(tmpFile) )
+      {
+          KIO::NetAccess::removeTempFile( tmpFile );
+          bool bCanSave = true;
+          // Create .mospics/large if it doesn't exist
+          KURL dirURL( m_url );
+          dirURL.setPath( m_url.directory() + "/.mospics" );
+          if (!KIO::NetAccess::exists( dirURL ))
+          {
+              bCanSave = KIO::NetAccess::mkdir( dirURL );
+          }
+          dirURL.setPath( m_url.directory() + sizeStr );
+          if (bCanSave && !KIO::NetAccess::exists( dirURL ))
+          {
+              bCanSave = KIO::NetAccess::mkdir( dirURL );
+          }
+          int w = pix.width(), h = pix.height();
+          // scale to pixie size
+          QImage img;
+          if(pix.width() > extent || pix.height() > extent){
+              if(pix.width() > pix.height()){
+                  float percent = (((float)extent)/pix.width());
+                  h = (int)(pix.height()*percent);
+                  w = extent;
               }
+              else{
+                  float percent = (((float)extent)/pix.height());
+                  w = (int)(pix.width()*percent);
+                  h = extent;
+              }
+              img = pix.convertToImage().smoothScale( w, h );
+              pix.convertFromImage( img );
+          }
+          else if(bCanSave)
+              img = pix.convertToImage();
+
+          if (bCanSave)
+          {
+              // write
+              QString tmpFile;
+              if ( thumbURL.isLocalFile() )
+              {
+                  tmpFile = thumbURL.path();
+              } else {
+                  KTempFile temp;
+                  if (temp.status() != 0)
+                      return pix;
+                  tmpFile = temp.name();
+              }
+              QImageIO iio;
+              iio.setImage(img);
+              iio.setFileName( tmpFile );
+              iio.setFormat("PNG");
+              if ( iio.write() )
+                  if ( !thumbURL.isLocalFile() )
+                  {
+                      KIO::NetAccess::upload( tmpFile, thumbURL );
+                      (void) ::unlink( tmpFile );
+                  }
+              return pix;
           }
       }
   }
