0ceea51b60e23b2d62d45f7f0406b4c4355aa111
David Faure <faure@kde.org>
2000-06-16 13:50:19 +0000

    Bye bye KonqURLEnterEvent.
    svn path=/trunk/kdebase/libkonq/; revision=53416

--- lib/konq/konq_events.cc
+++ lib/konq/konq_events.cc
@@ -1,6 +1,4 @@
 
 #include "konq_events.h"
 
-const char *KonqURLEnterEvent::s_urlEnterEventName = "Konqueror/URLEntered";
-
 const char *KonqFileSelectionEvent::s_fileItemSelectionEventName = "Konqueror/FileSelection";
--- lib/konq/konq_events.h
+++ lib/konq/konq_events.h
@@ -12,21 +12,6 @@ namespace KParts
 class KFileItem;
 typedef QList<KFileItem> KFileItemList;
 
-class KonqURLEnterEvent : public KParts::Event
-{
-public:
-  KonqURLEnterEvent( const QString &url ) : KParts::Event( s_urlEnterEventName ), m_url( url ) {}
-
-  QString url() const { return m_url; }
-
-  static bool test( const QEvent *event ) { return KParts::Event::test( event, s_urlEnterEventName ); }
-
-private:
-  static const char *s_urlEnterEventName;
-
-  QString m_url;
-};
-
 class KonqFileSelectionEvent : public KParts::Event
 {
 public:
