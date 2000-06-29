ce4af13fc9e43b8d2fd2c0fdd28d42fc8f734831
Simon Hausmann <hausmann@kde.org>
2000-06-29 08:36:42 +0000

    - lgpl
    svn path=/trunk/kdebase/libkonq/; revision=54838

--- lib/konq/konq_undo.cc
+++ lib/konq/konq_undo.cc
@@ -1,3 +1,21 @@
+/* This file is part of the KDE project
+   Copyright (C) 2000 Simon Hausmann <hausmann@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #include "konq_undo.h"
 
@@ -45,13 +63,13 @@ KonqCommandRecorder::KonqCommandRecorder( KonqCommand::Type op, const KURL::List
 
   connect( job, SIGNAL( copyingDone( KIO::Job *, const KURL &, const KURL &, bool, bool ) ),
 	   this, SLOT( slotCopyingDone( KIO::Job *, const KURL &, const KURL &, bool, bool ) ) );
-  
+
   KonqUndoManager::incRef();
 };
 
 KonqCommandRecorder::~KonqCommandRecorder()
 {
-  KonqUndoManager::decRef(); 
+  KonqUndoManager::decRef();
   delete d;
 }
 
