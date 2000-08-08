cdad38a384b98d127857f652d01543b20786fba6
David Faure <faure@kde.org>
2000-08-08 19:01:20 +0000

    Ctrl+D makes "Duplicate Window" much more useful
    svn path=/trunk/kdebase/konqueror/; revision=60215

--- konqueror/konq_mainwindow.cc
+++ konqueror/konq_mainwindow.cc
@@ -2073,7 +2073,7 @@ void KonqMainWindow::initActions()
   m_paFileType = new KAction( i18n( "Edit File Type..." ), 0, actionCollection(), "editMimeType" );
   m_paProperties = new KAction( i18n( "Properties..." ), 0, actionCollection(), "properties" );
   (void) new KAction( i18n( "New &Window" ), "window_new", KStdAccel::key(KStdAccel::New), this, SLOT( slotNewWindow() ), actionCollection(), "new_window" );
-  (void) new KAction( i18n( "&Duplicate Window" ), "window_new", 0 /*conflict with New Window! KStdAccel::key(KStdAccel::New)*/,
+  (void) new KAction( i18n( "&Duplicate Window" ), "window_new", CTRL+Key_D,
                       this, SLOT( slotDuplicateWindow() ), actionCollection(), "duplicate_window" );
 
   (void) new KAction( i18n( "&Run Command..." ), "run", 0/*kdesktop has a binding for it*/, this, SLOT( slotRun() ), actionCollection(), "run" );
