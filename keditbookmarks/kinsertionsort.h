bef887412083566943381bfd128c2eadeee63c6d
David Faure <faure@kde.org>
2000-11-21 13:56:48 +0000

    docu fixes
    svn path=/trunk/kdebase/konqueror/keditbookmarks/; revision=71625

--- keditbookmarks/kinsertionsort.h
+++ keditbookmarks/kinsertionsort.h
@@ -1,3 +1,20 @@
+/* This file is part of the KDE project
+   Copyright (C) 2000 David Faure <faure@kde.org>
+ 
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License version 2 as published by the Free Software Foundation.
+ 
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+ 
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #ifndef __kinsertionsort_h
 #define __kinsertionsort_h
