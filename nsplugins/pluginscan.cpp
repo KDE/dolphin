06465745689f82c69c38e9f8d60d297ec701ccef
Ivan E. Moore II <rkrusty@debian.org>
2000-11-25 15:58:18 +0000

    [IEM] adding in a couple new plugin search paths for debian systems
    svn path=/trunk/kdebase/nsplugins/; revision=72133

--- nsplugins/pluginscan.cpp
+++ nsplugins/pluginscan.cpp
@@ -302,6 +302,8 @@ QStringList getSearchPaths()
         paths.append("/opt/netscape/plugins");
         paths.append("/opt/netscape/communicator/plugins");
         paths.append("/usr/lib/netscape/plugins");
+        paths.append("/usr/lib/netscape/plugins-libc5");
+        paths.append("/usr/lib/netscape/plugins-libc6");
         paths.append("$MOZILLA_HOME/plugins");
         config->writeEntry( "scanPaths", paths );
     }
