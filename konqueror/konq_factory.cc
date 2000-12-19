8963260d18c1298ca6f6456149100e6a079a096a
Dawit Alemayehu <adawit@kde.org>
2000-12-19 13:44:54 +0000

    - Me too, authors update :) (DA)
    svn path=/trunk/kdebase/konqueror/; revision=75268

--- konqueror/konq_factory.cc
+++ konqueror/konq_factory.cc
@@ -211,6 +211,7 @@ const KAboutData *KonqFactory::aboutData()
     s_aboutData->addAuthor( "Harri Porten", I18N_NOOP("developer (JavaScript)"), "porten@kde.org" );
     s_aboutData->addAuthor( "Stefan Schimanski", I18N_NOOP("developer (Netscape plugin support)"), "1Stein@gmx.de" );
     s_aboutData->addAuthor( "George Staikos", I18N_NOOP("developer (SSL support)"), "staikos@kde.org" );
+    s_aboutData->addAuthor( "Dawit Alemayehu",I18N_NOOP("developer (I/O lib, Authentication support)"), "adawit@kde.org" );
   }
   return s_aboutData;
 }
