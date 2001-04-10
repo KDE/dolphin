9b73f4c41e31f9266edbd1e98b1b28cb72140894
David Faure <faure@kde.org>
2001-04-10 22:24:30 +0000

    kdFatal -> kdError (if called with wrong arguments)
    svn path=/trunk/kdebase/konqueror/; revision=91400

--- konqueror/kfmexec/main.cpp
+++ konqueror/kfmexec/main.cpp
@@ -268,7 +268,7 @@ int main( int argc, char **argv )
 
     if ( argc < 2 )
     {
-        kdFatal() << i18n( "Syntax Error:\nkfmexec command [URLs ....]" ) << endl;
+        kdError() << i18n( "Syntax Error:\nkfmexec command [URLs ....]" ) << endl;
         exit(1);
     }
 
