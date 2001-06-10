90d5173e55d947662d6b41acb2f327aa11b50566
Dawit Alemayehu <adawit@kde.org>
2001-06-10 12:22:13 +0000

    - Layout changes. (DA)
    svn path=/trunk/kdebase/kcontrol/kio/; revision=101312

--- konqueror/settings/kio/netpref.cpp
+++ konqueror/settings/kio/netpref.cpp
@@ -20,13 +20,9 @@
 KIOPreferences::KIOPreferences( QWidget* parent,  const char* name )
                :KCModule( parent, name )
 {
-    setCaption( i18n( "Preferences" ) );
-    QVBoxLayout* KIOPreferencesLayout = new QVBoxLayout( this );
-    KIOPreferencesLayout->setSpacing( 2*KDialog::spacingHint() );
-    KIOPreferencesLayout->setMargin( KDialog::marginHint() );
-
-    gb_Timeout = new QGroupBox( this, "gb_Timeout" );
-    gb_Timeout->setTitle( i18n( "Timeout Values" ) );
+    QVBoxLayout* mainLayout = new QVBoxLayout( this, 1.9*KDialog::marginHint(),
+                                               KDialog::spacingHint() );
+    gb_Timeout = new QGroupBox( i18n("Timeout Values"), this, "gb_Timeout" );
     gb_Timeout->setColumnLayout(0, Qt::Vertical );
     gb_Timeout->layout()->setSpacing( 0 );
     gb_Timeout->layout()->setMargin( 0 );
@@ -34,88 +30,89 @@ KIOPreferences::KIOPreferences( QWidget* parent,  const char* name )
 				       "You might want to tweak them if "
 				       "your connection is very slow." ) );
 
-    QGridLayout* gb_TimeoutLayout = new QGridLayout( gb_Timeout->layout() );
+    QVBoxLayout* gb_TimeoutLayout = new QVBoxLayout( gb_Timeout->layout() );
     gb_TimeoutLayout->setAlignment( Qt::AlignTop );
-    gb_TimeoutLayout->setSpacing( KDialog::spacingHint() );
+    gb_TimeoutLayout->setSpacing( 6 );
     gb_TimeoutLayout->setMargin( 11 );
 
-    QVBoxLayout* vbox = new QVBoxLayout;
-    vbox->setSpacing( KDialog::spacingHint() );
-    vbox->setMargin( 0 );
+    QGridLayout* grid_topLevel = new QGridLayout;
+    grid_topLevel->setSpacing( KDialog::spacingHint() );
+    grid_topLevel->setMargin( 1.9*KDialog::marginHint() );
 
-    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-    spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-    spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-    spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
+    QGridLayout* grid_firstColumn = new QGridLayout;
+    grid_firstColumn->setSpacing( KDialog::spacingHint() );
+    grid_firstColumn->setMargin( 0 );
+    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Fixed, QSizePolicy::Minimum );
+    grid_firstColumn->addItem( spacer, 0, 1 );
 
-    gb_TimeoutLayout->addLayout( vbox, 0, 3 );
+    QVBoxLayout* vlay_firstColumnLabels = new QVBoxLayout;
+    vlay_firstColumnLabels->setSpacing( KDialog::spacingHint() );
+    vlay_firstColumnLabels->setMargin( 0 );
 
+    QLabel* lbl_socket = new QLabel( i18n( "Soc&ket Read" ), gb_Timeout,
+                                     "lbl_socket" );
+    vlay_firstColumnLabels->addWidget( lbl_socket );
+    QLabel* lbl_proxy = new QLabel( i18n( "Pro&xy Connect" ), gb_Timeout,
+                            "lbl_proxy" );
+    vlay_firstColumnLabels->addWidget( lbl_proxy );
 
-    vbox = new QVBoxLayout;
-    vbox->setSpacing( KDialog::spacingHint() );
-    vbox->setMargin( 0 );
+    grid_firstColumn->addLayout( vlay_firstColumnLabels, 0, 0 );
 
-    QLabel* lbl_socket = new QLabel( gb_Timeout, "lbl_socket" );
-    lbl_socket->setText( i18n( "Soc&ket Read:" ) );
-    vbox->addWidget( lbl_socket );
-
-    QLabel* lbl_proxy = new QLabel( gb_Timeout, "lbl_proxy" );
-    lbl_proxy->setText( i18n( "Pro&xy Connect:" ) );
-    vbox->addWidget( lbl_proxy );
-
-    QLabel* lbl_serverConnect = new QLabel( gb_Timeout, "lbl_serverConnect" );
-    lbl_serverConnect->setText( i18n( "Server Co&nnect:" ) );
-    vbox->addWidget( lbl_serverConnect );
-
-    QLabel* lbl_serverResponse = new QLabel( gb_Timeout, "lbl_serverResponse" );
-    lbl_serverResponse->setText( i18n( "Server &Response:" ) );
-    vbox->addWidget( lbl_serverResponse );
-
-    gb_TimeoutLayout->addLayout( vbox, 0, 0 );
-
-    vbox = new QVBoxLayout;
-    vbox->setSpacing( KDialog::spacingHint() );
-    vbox->setMargin( 0 );
-    spacer = new QSpacerItem( 28, 20, QSizePolicy::Fixed, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-    spacer = new QSpacerItem( 28, 20, QSizePolicy::Fixed, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-    spacer = new QSpacerItem( 28, 20, QSizePolicy::Fixed, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-    spacer = new QSpacerItem( 28, 20, QSizePolicy::Fixed, QSizePolicy::Minimum );
-    vbox->addItem( spacer );
-
-    gb_TimeoutLayout->addLayout( vbox, 0, 1 );
-
-    vbox = new QVBoxLayout;
-    vbox->setSpacing( KDialog::spacingHint() );
-    vbox->setMargin( 0 );
+    QVBoxLayout* vlay_firstColumnSpinBox = new QVBoxLayout;
+    vlay_firstColumnSpinBox->setSpacing( KDialog::spacingHint() );
+    vlay_firstColumnSpinBox->setMargin( 0 );
 
     sb_socketRead = new QSpinBox( gb_Timeout, "sb_socketRead" );
     sb_socketRead->setSuffix( i18n( "    sec" ) );
-    vbox->addWidget( sb_socketRead );
+    vlay_firstColumnSpinBox->addWidget( sb_socketRead );
 
     sb_proxyConnect = new QSpinBox( gb_Timeout, "sb_proxyConnect" );
     sb_proxyConnect->setSuffix( i18n( "    sec" ) );
-    vbox->addWidget( sb_proxyConnect );
+    vlay_firstColumnSpinBox->addWidget( sb_proxyConnect );
+
+    grid_firstColumn->addLayout( vlay_firstColumnSpinBox, 0, 2 );
+
+    grid_topLevel->addLayout( grid_firstColumn, 0, 0 );
+    spacer = new QSpacerItem( 20, 20, QSizePolicy::Preferred, QSizePolicy::Minimum );
+    grid_topLevel->addItem( spacer, 0, 3 );
+
+    QGridLayout* grid_secondColumn = new QGridLayout;
+    grid_secondColumn->setSpacing( KDialog::spacingHint() );
+    grid_secondColumn->setMargin( 0 );
+
+    QVBoxLayout* vlay_secondColumnLabel = new QVBoxLayout;
+    vlay_secondColumnLabel->setSpacing( KDialog::spacingHint() );
+    vlay_secondColumnLabel->setMargin( 0 );
+    QLabel* lbl_serverConnect = new QLabel( i18n("Server Co&nnect"), gb_Timeout,
+                                            "lbl_serverConnect" );
+    vlay_secondColumnLabel->addWidget( lbl_serverConnect );
+    QLabel* lbl_serverResponse = new QLabel( i18n("Server &Response"), gb_Timeout,
+                                             "lbl_serverResponse" );
+    vlay_secondColumnLabel->addWidget( lbl_serverResponse );
+    grid_secondColumn->addLayout( vlay_secondColumnLabel, 0, 0 );
+
+    QVBoxLayout* vlay_secondColumnSpinBox = new QVBoxLayout;
+    vlay_secondColumnSpinBox->setSpacing( KDialog::spacingHint() );
+    vlay_secondColumnSpinBox->setMargin( 0 );
 
     sb_serverConnect = new QSpinBox( gb_Timeout, "sb_serverConnect" );
-    sb_serverConnect->setSuffix( i18n( "    sec" ) );
-    vbox->addWidget( sb_serverConnect );
+    sb_serverConnect->setSuffix( i18n( "    secs" ) );
+    vlay_secondColumnSpinBox->addWidget( sb_serverConnect );
 
     sb_serverResponse = new QSpinBox( gb_Timeout, "sb_serverResponse" );
-    sb_serverResponse->setSuffix( i18n( "    sec" ) );
-    vbox->addWidget( sb_serverResponse );
+    sb_serverResponse->setSuffix( i18n( "    secs" ) );
+    vlay_secondColumnSpinBox->addWidget( sb_serverResponse );
 
-    gb_TimeoutLayout->addLayout( vbox, 0, 2 );
-    KIOPreferencesLayout->addWidget( gb_Timeout );
+    grid_secondColumn->addLayout( vlay_secondColumnSpinBox, 0, 2 );
 
-    spacer = new QSpacerItem( 1, 100, QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding ); 
-    KIOPreferencesLayout->addItem( spacer );
+    spacer = new QSpacerItem( 16, 20, QSizePolicy::Fixed, QSizePolicy::Minimum );
+    grid_secondColumn->addItem( spacer, 0, 1 );
+
+    grid_topLevel->addLayout( grid_secondColumn, 0, 2 );
+    spacer = new QSpacerItem( 20, 20, QSizePolicy::Preferred, QSizePolicy::Minimum );
+    grid_topLevel->addItem( spacer, 0, 1 );
+    gb_TimeoutLayout->addLayout( grid_topLevel );
+    mainLayout->addWidget( gb_Timeout );
 
     lbl_socket->setBuddy( sb_socketRead );
     lbl_proxy->setBuddy( sb_proxyConnect );
