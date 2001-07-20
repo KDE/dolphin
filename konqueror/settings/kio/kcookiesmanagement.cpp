9c80de567dc43b12000dbf0166cd20e72249a323
Dawit Alemayehu <adawit@kde.org>
2001-07-20 08:23:32 +0000

    - Add license. (DA)
    svn path=/trunk/kdebase/kcontrol/kio/; revision=106938

--- konqueror/settings/kio/kcookiesmanagement.cpp
+++ konqueror/settings/kio/kcookiesmanagement.cpp
@@ -1,7 +1,25 @@
-// kcookiesmanagement.cpp - View/delete received cookies
-//
-// Original framework by Waldo, David, Dawit
-// This dialog created by Marco Pinelli <pinmc@orion.it>
+/**
+ * kcookiesmanagement.cpp - Cookies manager
+ *
+ * Copyright 2000-2001 Marco Pinelli <pinmc@orion.it>
+ *
+ * Contributors:
+ * Copyright (c) 2000-2001 Dawit Alemayehu <adawit@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <qlayout.h>
 #include <qwhatsthis.h>
@@ -79,7 +97,7 @@ KCookiesManagement::KCookiesManagement(QWidget *parent, const char *name)
                    :KCModule(parent, name)
 {
   // Toplevel layout
-  QVBoxLayout *layout = new QVBoxLayout( this, KDialog::marginHint(),
+  QVBoxLayout *layout = new QVBoxLayout( this, 2*KDialog::marginHint(),
                                          KDialog::spacingHint() );
   layout->setAutoAdd( true );
 
@@ -414,8 +432,7 @@ bool KCookiesManagement::getCookieDetails(CookieProp *cookie)
     cookie->allLoaded = true;
     return true;
   }
-  else
-    return false;
+  return false;
 }
 
 void KCookiesManagement::showCookieDetails(QListViewItem* item)
--- konqueror/settings/kio/kcookiesmanagement.h
+++ konqueror/settings/kio/kcookiesmanagement.h
@@ -1,6 +1,26 @@
-/*
-    Copyright 2000-2001 Marco Pinelli <pinmc@orion.it>
-*/
+/**
+ * kcookiesmanagement.h - Cookies manager
+ *
+ * Copyright 2000-2001 Marco Pinelli <pinmc@orion.it>
+ *
+ * Contributors:
+ * Copyright (c) 2000-2001 Dawit Alemayehu <adawit@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef __KCOOKIESMANAGEMENT_H
 #define __KCOOKIESMANAGEMENT_H
 
