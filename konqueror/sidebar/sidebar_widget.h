406361d46b10a705f391e037ecc5bda3bdb97403
Joseph Wenninger <kde@jowenn.at>
2001-07-22 17:58:15 +0000

    Finally added copyright notices
    svn path=/trunk/kdebase/konqueror/; revision=107197

--- konqueror/sidebar/sidebar_widget.h
+++ konqueror/sidebar/sidebar_widget.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                               sidebar_widget.h
+                             -------------------
+    begin                : Sat June 2 16:25:27 CEST 2001
+    copyright            : (C) 2001 Joseph Wenninger
+    email                : jowenn@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef _SIDEBAR_WIDGET_
 #define _SIDEBAR_WIDGET_
 #include <qhbox.h>
