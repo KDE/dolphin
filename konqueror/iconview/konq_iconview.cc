2e748c330c18b8b626bc01bab019601aef3dc0a3
Waldo Bastian <bastian@kde.org>
2001-08-18 22:13:32 +0000

    Large file support
    svn path=/trunk/kdebase/konqueror/; revision=110953

--- konqueror/iconview/konq_iconview.cc
+++ konqueror/iconview/konq_iconview.cc
@@ -958,7 +958,7 @@ void KonqKfmIconView::setupSortKeys()
 
 QString KonqKfmIconView::makeSizeKey( KFileIVI *item )
 {
-    return QString::number( item->item()->size() ).rightJustify( 20, '0' );
+    return KIO::number( item->item()->size() ).rightJustify( 20, '0' );
 }
 
 void KonqKfmIconView::disableIcons( const KURL::List & lst )
