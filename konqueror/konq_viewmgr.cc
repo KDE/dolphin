e136e5a3ac5f134ff9d2a4b19a979ecabaa099b2
Simon Hausmann <hausmann@kde.org>
2001-09-04 18:57:44 +0000

    - explicitly show the main container
    svn path=/trunk/kdebase/konqueror/; revision=113322

--- konqueror/konq_viewmgr.cc
+++ konqueror/konq_viewmgr.cc
@@ -215,7 +215,7 @@ KonqView* KonqViewManager::split (KonqFrameBase* splitFrame,
 
     childView = setupView( m_pMainContainer, newViewFactory, service, partServiceOffers, appServiceOffers, serviceType, passiveMode );
 
-    //m_pMainContainer->show();
+    m_pMainContainer->show();
 
     //childView->frame()->statusbar()->hideStuff();
 
