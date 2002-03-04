a9b175f9a03a87168f680a12c110354c4b259cc4
Carsten Pfeiffer <pfeiffer@kde.org>
2002-03-04 23:53:51 +0000

    centralize Konq's KBookmarkManager
    svn path=/trunk/kdebase/libkonq/; revision=141029

---
+++ lib/konq/konqbookmarkmanager.h
@@ -0,0 +1,20 @@
+#include <kbookmarkmanager.h>
+#include <kstandarddirs.h>
+
+class KonqBookmarkManager
+{
+public:
+    static KBookmarkManager * self() {
+        if ( !s_bookmarkManager )
+        {
+            QString bookmarksFile = locateLocal("data", QString::fromLatin1("konqueror/bookmarks.xml"));
+            s_bookmarkManager = KBookmarkManager::self( bookmarksFile );
+        }
+        return s_bookmarkManager;
+    }
+
+
+    static KBookmarkManager *s_bookmarkManager;
+};
+
+KBookmarkManager * KonqBookmarkManager::s_bookmarkManager;
