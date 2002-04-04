92d66d4831cfc7aeea9e455767810be9f35f265d
Carsten Pfeiffer <pfeiffer@kde.org>
2002-04-04 18:18:58 +0000

    F6 as default shortcut for "locationCombo->setFocus()". Also highlight the locationbar's contents then.
    (any objections?)
    
    svn path=/trunk/kdebase/konqueror/; revision=147096

--- konqueror/konq_mainwindow.cc
+++ konqueror/konq_mainwindow.cc
@@ -2824,7 +2824,7 @@ void KonqMainWindow::initActions()
   m_paAnimatedLogo = new KonqLogoAction( i18n("Animated Logo"), 0, this, SLOT( slotDuplicateWindow() ), actionCollection(), "animated_logo" );
 
   // Location bar
-  (void)new KonqLabelAction( i18n( "L&ocation: " ), 0, this, SLOT( slotLocationLabelActivated() ), actionCollection(), "location_label" );
+  (void)new KonqLabelAction( i18n( "L&ocation: " ), Key_F6, this, SLOT( slotLocationLabelActivated() ), actionCollection(), "location_label" );
 
   m_paURLCombo = new KonqComboAction( i18n( "Location Bar" ), 0, this, SLOT( slotURLEntered( const QString & ) ), actionCollection(), "toolbar_url_combo" );
   m_paURLCombo->setShortcutConfigurable( false );
@@ -3755,8 +3755,10 @@ void KonqMainWindow::goURL()
 
 void KonqMainWindow::slotLocationLabelActivated()
 {
-    if ( m_combo )
+    if ( m_combo ) {
         m_combo->setFocus();
+        m_combo->lineEdit()->selectAll();
+    }
 }
 
 void KonqMainWindow::slotOpenURL( const KURL& url )
