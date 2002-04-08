ef7421ee39a2b66f177db18a290bf3defbc44388
Ariya Hidayat <ariya@kde.org>
2002-04-08 01:48:11 +0000

    Leightweight -> Lightweight
    svn path=/trunk/kdebase/kate/app/kwritemain.cpp; revision=147760

--- kwrite/kwritemain.cpp
+++ kwrite/kwritemain.cpp
@@ -515,7 +515,7 @@ int main(int argc, char **argv)
   KLocale::setMainCatalogue("kate");         //lukas: set this to have the kwritepart translated using kate message catalog
 
   KAboutData aboutData ("kwrite", I18N_NOOP("KWrite"), "4.0",
-	I18N_NOOP( "KWrite - Leightweight Kate" ), KAboutData::License_GPL,
+	I18N_NOOP( "KWrite - Lightweight Kate" ), KAboutData::License_GPL,
 	I18N_NOOP( "(c) 2000-2001 The Kate Authors" ), 0, "http://kate.kde.org");
 
   aboutData.addAuthor("Christoph Cullmann", I18N_NOOP("Project Manager and Core Developer"), "cullmann@kde.org", "http://www.babylon2k.de");
