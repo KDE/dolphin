1116939fb37869ca247f996b30f0121540ff205b
Dawit Alemayehu <adawit@kde.org>
2002-05-27 07:48:16 +0000

    Make it compile. (DA)
    svn path=/trunk/kdebase/kate/app/kwritemain.cpp; revision=158009

--- kwrite/kwritemain.cpp
+++ kwrite/kwritemain.cpp
@@ -411,14 +411,14 @@ void KWrite::restore(KConfig *config, int n)
 void KWrite::readProperties(KConfig *config)
 {
   readConfig(config);
-  KTextEditor::sessionConfigInterface(kateView)->readSessionConfig(config);
+  KTextEditor::sessionConfigInterface(kateView->document())->readSessionConfig(config);
 }
 
 void KWrite::saveProperties(KConfig *config)
 {
   writeConfig(config);
   config->writeEntry("DocumentNumber",docList.find(kateView->document()) + 1);
-  KTextEditor::sessionConfigInterface(kateView)->writeSessionConfig(config);
+  KTextEditor::sessionConfigInterface(kateView->document())->writeSessionConfig(config);
 }
 
 void KWrite::saveGlobalProperties(KConfig *config) //save documents
