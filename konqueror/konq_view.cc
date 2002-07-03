45d7df86cc23b1289bb9aef8e28b00c5903fd74b
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2002-07-03 01:18:11 +0000

    Corrected typographical errors
    svn path=/trunk/kdebase/kcontrol/kio/; revision=164481

--- konqueror/konq_view.cc
+++ konqueror/konq_view.cc
@@ -1099,7 +1099,7 @@ bool KonqView::prepareReload( KParts::URLArgs& args )
     if ( m_doPost )
     {
         if ( KMessageBox::warningContinueCancel( 0, i18n(
-				"The page you are trying to view is the result of a posted formular. "
+				"The page you are trying to view is the result of a posted formula. "
 				"If you resend the data, any action the form carried out (such as search or online purchase) will be repeated. "),
                                                  i18n( "Security Warning" ), i18n( "Resend" ) ) == KMessageBox::Continue )
         {
