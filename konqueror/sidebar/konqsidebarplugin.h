44c5bc7df7bc8f1fadf9bb78b7b35da9558249c3
Joseph Wenninger <kde@jowenn.at>
2002-07-27 12:47:26 +0000

    Make the plugin interface __BC__ to KDE 3.0 again
    svn path=/trunk/kdebase/konqueror/; revision=169054

--- konqueror/sidebar/konqsidebarplugin.h
+++ konqueror/sidebar/konqsidebarplugin.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
-   Copyright (C) 2001 Joseph Wenninger <jowenn@kde.org>
+   Copyright (C) 2001,2002 Joseph Wenninger <jowenn@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
