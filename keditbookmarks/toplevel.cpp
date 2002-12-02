34399b812ea19800fc38186fe4d4c6ba0453bd31
Alexander Kellett <lypanov@kde.org>
2002-12-02 17:54:15 +0000

    show "configure ..." options again
    svn path=/trunk/kdebase/konqueror/keditbookmarks/; revision=191662

--- keditbookmarks/toplevel.cpp
+++ keditbookmarks/toplevel.cpp
@@ -170,7 +170,7 @@ void KEBTopLevel::resetActions()
    for (; it != end; ++it) {
       KAction *act = *it;
       // do not touch the configureblah actions
-      if ( strncmp(act->name(), "configure", 9) )
+      if ( strncmp(act->name(), "options_configure", 9) )
          act->setEnabled(false);
    }
 
