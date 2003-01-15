a6708cb00b3fbd35f6beac665bde8ab7e79d47c3
Carsten Pfeiffer <pfeiffer@kde.org>
2003-01-15 00:03:34 +0000

    remove the commented out stuff...
    svn path=/trunk/kdebase/konqueror/; revision=199986

--- konqueror/konq_combo.cc
+++ konqueror/konq_combo.cc
@@ -297,12 +297,6 @@ bool KonqCombo::eventFilter( QObject *o, QEvent *ev )
         if ( type == QEvent::KeyPress ) {
             QKeyEvent *e = static_cast<QKeyEvent *>( ev );
 
-//             if (e->key() == Key_Return || e->key() == Key_Enter) {
-//                 emit activated( currentText(), e->state() );
-//                 e->accept();
-//                 return true;
-//             }
-
             if ( KStdAccel::isEqual( e, KStdAccel::deleteWordBack() ) ||
                  KStdAccel::isEqual( e, KStdAccel::deleteWordForward() ) ||
                  ((e->state() & ControlButton) &&
