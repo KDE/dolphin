c5065df89370494f675316c02f90c0b2778e6127
George Staikos <staikos@kde.org>
2003-03-04 20:58:42 +0000

    A disgusting hack to get more plugins to work.  this fixes real player plugin. Some plugins don't resize when the motif canvas resizes.  So what we do is resize them ourself by walking the window tree underneath the canvas and explicitly XResizeWindow()ing whatever we find.
    Please test with all your favorite plugins to see if I broke or fixed
    anything.
    
    CCMAIL: 39685-done@bugs.kde.org
    
    svn path=/trunk/kdebase/nsplugins/; revision=211381

--- nsplugins/viewer/nsplugin.cpp
+++ nsplugins/viewer/nsplugin.cpp
@@ -744,6 +744,20 @@ int NSPluginInstance::setWindow(int remove)
 }
 
 
+static void resizeWidgetTree(Window w, int width, int height) {
+   Window rroot, parent, *children;
+   unsigned int nchildren = 0;
+
+   if (XQueryTree(qt_xdisplay(), w, &rroot, &parent, &children, &nchildren)) {
+      for (unsigned int i = 0; i < nchildren; i++) {
+         XResizeWindow(qt_xdisplay(), children[i], width, height);
+         resizeWidgetTree(children[i], width, height);
+      }
+   }
+
+}
+
+
 void NSPluginInstance::resizePlugin(int w, int h)
 {
    if (!_visible)
@@ -761,6 +775,8 @@ void NSPluginInstance::resizePlugin(int w, int h)
 
    XtSetValues(_form, args, nargs);
    XtSetValues(_area, args, nargs);
+
+   resizeWidgetTree(XtWindow(_area), _width, _height);
 
    setWindow();
 
