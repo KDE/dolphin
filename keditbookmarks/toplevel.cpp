24272ada25375e2e3e0fcfbea1b45bc54a006c91
Alexander Kellett <lypanov@kde.org>
2003-06-09 11:38:22 +0000

    bookmarkinfo stuff : add showbookmark + add moddate,credate
    svn path=/trunk/kdebase/konqueror/keditbookmarks/; revision=230552

--- keditbookmarks/toplevel.cpp
+++ keditbookmarks/toplevel.cpp
@@ -246,20 +246,39 @@ public:
    BookmarkInfoWidget(QWidget * parent = 0, const char * name = 0)
     : QWidget (parent, name) {
       QBoxLayout *vbox = new QVBoxLayout(this);
-      QGridLayout *grid = new QGridLayout( vbox, 2, 2 );
+      QGridLayout *grid = new QGridLayout( vbox, 3, 4 );
 
       m_title_le = new KLineEdit( this );
-      m_title_le->setText( m_title );
       grid->addWidget( m_title_le, 0, 1 );
       grid->addWidget( new QLabel( m_title_le, i18n( "Name:" ), this ), 0, 0 );
 
       m_url_le = new KLineEdit( this );
-      m_url_le->setText( m_url );
       grid->addWidget( m_url_le, 1, 1 );
       grid->addWidget( new QLabel( m_url_le, i18n( "Location:" ), this ), 1, 0 );
+
+      m_comment_le = new KLineEdit( this );
+      grid->addWidget( m_comment_le, 2, 1 );
+      grid->addWidget( new QLabel( m_comment_le, i18n( "Comment:" ), this ), 2, 0 );
+
+      m_moddate_le = new KLineEdit( this );
+      grid->addWidget( m_moddate_le, 0, 3 );
+      grid->addWidget( new QLabel( m_moddate_le, i18n( "First viewed:" ), this ), 0, 2 );
+
+      m_credate_le = new KLineEdit( this );
+      grid->addWidget( m_credate_le, 1, 3 );
+      grid->addWidget( new QLabel( m_credate_le, i18n( "Viewed last:" ), this ), 1, 2 );
    }
-   KLineEdit *m_title_le, *m_url_le;
-   QString m_title, m_url;
+public:
+   void showBookmark(const KBookmark &bk) {
+      m_title_le->setText(bk.text());
+      m_url_le->setText(bk.url().url());
+      m_comment_le->setText("a comment");
+      m_moddate_le->setText("the modification date");
+      m_credate_le->setText("the creation date");
+   }
+private:
+   KLineEdit *m_title_le, *m_url_le, *m_comment_le, *m_moddate_le, *m_credate_le;
+   KBookmark m_bk;
 };
 
 KEBApp::KEBApp(const QString & bookmarksFile, bool readonly, const QString &address)
