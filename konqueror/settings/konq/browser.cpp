f60818b6db378d02b74b549c05a882dd072b08a2
Nadeem Hasan <nhasan@kde.org>
2003-08-19 21:46:05 +0000

    Use the new KCModuleLoader::loadModule() to cut down tons of duplicated code.
    svn path=/trunk/kdebase/kcontrol/kio/; revision=243662

--- konqueror/settings/konq/browser.cpp
+++ konqueror/settings/konq/browser.cpp
@@ -22,76 +22,12 @@
 
 #include <klocale.h>
 #include <kdialog.h>
-#include <kparts/componentfactory.h>
+#include <kcmoduleloader.h>
 
 #include "behaviour.h"
 #include "fontopts.h"
 #include "previews.h"
 #include "browser.h"
-
-static KCModule *load(QWidget *parent, const QString &libname, const QString &library, const QString &handle)
-{
-    KLibLoader *loader = KLibLoader::self();
-    // attempt to load modules with ComponentFactory, only if the symbol init_<lib> exists
-    // (this is because some modules, e.g. kcmkio with multiple modules in the library,
-    // cannot be ported to KGenericFactory)
-    KLibrary *lib = loader->library(QFile::encodeName(libname.arg(library)));
-    if (lib) {
-        QString initSym("init_");
-        initSym += libname.arg(library);
-
-        if ( lib->hasSymbol(QFile::encodeName(initSym)) )
-        {
-            // Reuse "lib" instead of letting createInstanceFromLibrary recreate it
-            //KCModule *module = KParts::ComponentFactory::createInstanceFromLibrary<KCModule>(QFile::encodeName(libname.arg(library)));
-            KLibFactory *factory = lib->factory();
-            if ( factory )
-            {
-                KCModule *module = KParts::ComponentFactory::createInstanceFromFactory<KCModule>( factory );
-                if (module)
-                    return module;
-            }
-        }
-    
-	// get the create_ function
-	QString factory("create_%1");
-	void *create = lib->symbol(QFile::encodeName(factory.arg(handle)));
-	
-	if (create)
-	    {
-		// create the module
-		KCModule* (*func)(QWidget *, const char *);
-		func = (KCModule* (*)(QWidget *, const char *)) create;
-		return  func(parent, 0);
-	    }
-
-        lib->unload();
-    }
-    return 0;
-}
-
-static KCModule *loadModule(QWidget *parent, const QString &module)
-{
-    KService::Ptr service = KService::serviceByDesktopName(module);
-    if (!service)
-       return 0;
-    QString library = service->library();
-    
-    if (library.isEmpty())
-       return 0;
-
-    QString handle =  service->property("X-KDE-FactoryName").toString();
-    if (handle.isEmpty())
-       handle = library;
-
-    KCModule *kcm = load(parent, "kcm_%1", library, handle);
-    if (!kcm)
-       kcm = load(parent, "libkcm_%1", library, handle);
-    return kcm;
-}
-
-
-//-----------------------------------------------------------------------------
 
 KBrowserOptions::KBrowserOptions(KConfig *config, QString group, QWidget *parent, const char *name)
     : KCModule( parent, "kcmkonq" ) 
@@ -109,7 +45,7 @@ KBrowserOptions::KBrowserOptions(KConfig *config, QString group, QWidget *parent
   previews = new KPreviewOptions(tab, name);
   previews->layout()->setMargin( KDialog::marginHint() );
 
-  kuick = loadModule(tab, "kcmkuick");
+  kuick = KCModuleLoader::loadModule("kcmkuick", tab);
 
   tab->addTab(appearance, i18n("&Appearance"));
   tab->addTab(behavior, i18n("&Behavior"));
