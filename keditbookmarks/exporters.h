713e94655ac6d53c93bcc622de305d35b6014482
Alexander Kellett <lypanov@kde.org>
2003-08-28 19:57:26 +0000

    add exporters.h, thanks martijn
    svn path=/trunk/kdebase/konqueror/keditbookmarks/; revision=246111

---
+++ keditbookmarks/exporters.h
@@ -0,0 +1,39 @@
+/* This file is part of the KDE project
+   Copyright (C) 2003 Alexander Kellett <lypanov@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __exporters_h
+#define __exporters_h
+
+#include <kbookmark.h>
+
+class HTMLExporter : private KBookmarkGroupTraverser {
+public:
+   HTMLExporter();
+   QString toString(const KBookmarkGroup &);
+   void write(const KBookmarkGroup &, const QString &);
+private:
+   virtual void visit(const KBookmark &);
+   virtual void visitEnter(const KBookmarkGroup &);
+   virtual void visitLeave(const KBookmarkGroup &);
+private:
+   QString m_string;
+   QTextStream m_out;
+   int m_level;
+};
+
+#endif
