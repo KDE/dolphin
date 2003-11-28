604f2092eb9c853d266e99f57e743cbda8728958
Waldo Bastian <bastian@kde.org>
2003-11-28 12:55:15 +0000

    Migrate favicons to new location.
    svn path=/trunk/kdebase/libkonq/; revision=270073

--- lib/konq/favicons/move_favicons.sh
+++ lib/konq/favicons/move_favicons.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-prefix=`kde-config --localprefix`
-source="$prefix/share/icons/favicons"
-dest="$prefix/share/cache/favicons"
+prefix=$(kde-config --localprefix)
+source1="$prefix/share/icons/favicons"
+source2="$prefix/share/cache/favicons"
+dest="$(kde-config --path cache)/favicons"
 
-if [ -n "$prefix" -a -d "$source" ]; then
+if [ -n "$prefix" -a -d "$source1" ]; then
 	while [ ! -d "$dest" ]; do
 		dir="$dest"
 		while [ ! -d `dirname "$dir"` ]; do
@@ -13,11 +14,28 @@ if [ -n "$prefix" -a -d "$source" ]; then
 		mkdir "$dir" || exit 1
 	done
 
-	icons=`ls "$source" 2>/dev/null`
+	icons=`ls "$source1" 2>/dev/null`
 	if [ -n "$icons" ]; then
 		for i in $icons; do
-			mv "$source/$i" "$dest/$i"
+			mv -f "$source1/$i" "$dest/$i"
 		done
 	fi
-	rmdir "$source"
+	rmdir "$source1"
+fi
+if [ -n "$prefix" -a -d "$source2" ]; then
+	while [ ! -d "$dest" ]; do
+		dir="$dest"
+		while [ ! -d `dirname "$dir"` ]; do
+			dir=`dirname "$dir"`
+		done
+		mkdir "$dir" || exit 1
+	done
+
+	icons=`ls "$source2" 2>/dev/null`
+	if [ -n "$icons" ]; then
+		for i in $icons; do
+			mv -f "$source2/$i" "$dest/$i"
+		done
+	fi
+	rmdir "$source2"
 fi
