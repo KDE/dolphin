25a9bdf31a03785ac0f6c02529902c019402a4a1
Andrew Coles <andrew.i.coles@googlemail.com>
2004-05-19 12:40:02 +0000

    Corrected typo (exclamation point usage).
    svn path=/trunk/kdebase/kate/app/kwritemain.cpp; revision=312601

--- kwrite/kwritemain.cpp
+++ kwrite/kwritemain.cpp
@@ -638,7 +638,7 @@ extern "C" int kdemain(int argc, char **argv)
             Kate::Document::setOpenErrorDialogsActivated (true);
         }
         else
-          KMessageBox::sorry( t, i18n("The file '%1' could not be opened, it is not a normal file, it is a folder!").arg(args->url(z).url()) );
+          KMessageBox::sorry( t, i18n("The file '%1' could not be opened: it is not a normal file, it is a folder.").arg(args->url(z).url()) );
       }
     }
   }
