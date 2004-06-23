e0d4d333e62d60b5b5dc7fe591ad4afff5a0616f
Andrew Coles <andrew.i.coles@googlemail.com>
2004-06-23 11:35:00 +0000

    CVS_SILENT
    Corrected typo.
    
    svn path=/trunk/kdebase/konqueror/; revision=323053

--- konqueror/konq_main.cc
+++ konqueror/konq_main.cc
@@ -41,7 +41,7 @@ static KCmdLineOptions options[] =
   { "preload", I18N_NOOP("Preload for later use"), 0 },
   { "profile <profile>",   I18N_NOOP("Profile to open"), 0 },
   { "profiles", I18N_NOOP("List available profiles"), 0 },
-  { "mimetype <mimetype>",   I18N_NOOP("Mimetype to use for this URL, (e.g. text/html or inode/directory)"), 0 },
+  { "mimetype <mimetype>",   I18N_NOOP("Mimetype to use for this URL (e.g. text/html or inode/directory)"), 0 },
   { "select", I18N_NOOP("For URLs that point to files, opens the directory and selects the file, instead of opening the actual file"), 0 },
   { "+[URL]",   I18N_NOOP("Location to open"), 0 },
   KCmdLineLastOption
