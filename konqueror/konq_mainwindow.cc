c8f9f1aa9d86bafff34f1aba03eafcb9418eaac9
Luboš Luňák <l.lunak@kde.org>
2004-07-13 14:38:04 +0000

    chdir to / when keeping for preloading, just in case cwd is a mounted device.
    svn path=/trunk/kdebase/konqueror/; revision=328714

--- konqueror/konq_mainwindow.cc
+++ konqueror/konq_mainwindow.cc
@@ -5219,6 +5219,7 @@ void KonqMainWindow::setPreloadedFlag( bool preloaded )
     s_preloaded = preloaded;
     if( s_preloaded )
     {
+        chdir("/"); // so that it's cwd is not e.g. subfs mounted device
         kapp->disableSessionManagement(); // dont restore preloaded konqy's
         return; // was registered before calling this
     }
