e4ffe90239077a6fa2ce65f438e5060c77b8ea4d
Laurent Montel <montel@kde.org>
2006-01-13 14:11:27 +0000

    read*entry--
    svn path=/trunk/KDE/kdebase/konqueror/; revision=497662

--- konqueror/settings/kio/socks.cpp
+++ konqueror/settings/kio/socks.cpp
@@ -199,7 +199,7 @@ void KSocksConfig::load()
 {
   KConfigGroup config(KGlobal::config(), "Socks");
   base->_c_enableSocks->setChecked(config.readEntry("SOCKS_enable", false));
-  int id = config.readNumEntry("SOCKS_method", 1);
+  int id = config.readEntry("SOCKS_method", 1);
   base->bg->setButton(id);
   if (id == 4) {
     base->_c_customLabel->setEnabled(true);
