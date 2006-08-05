b54b206d5e45441fbd1ae0fe3a053a5316f1e746
Nicolas Goutte <nicolasg@snafu.de>
2006-08-05 17:43:39 +0000

    There are no plugins directory anymore to extract messages
    svn path=/trunk/KDE/kdebase/konqueror/; revision=570099

--- konqueror/settings/ebrowsing/Messages.sh
+++ konqueror/settings/ebrowsing/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC plugins/*/*.ui >> rc.cpp || exit 11
-$XGETTEXT *.cpp plugins/ikws/*.cpp plugins/shorturi/*.cpp -o $podir/kcmkurifilt.pot
+$XGETTEXT *.cpp -o $podir/kcmkurifilt.pot
