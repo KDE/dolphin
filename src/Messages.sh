097a1fea8822a2539fafd32278eeff9dcbe11aeb
Chusslove Illich <caslav.ilic@gmx.net>
2007-07-13 14:34:42 +0000

    Extract messages from .kcfg too.
    svn path=/trunk/KDE/kdebase/apps/; revision=687406

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc` > rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -name \*.kcfg` > rc.cpp
 $XGETTEXT *.cpp -o $podir/dolphin.pot
 
