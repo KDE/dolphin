5d9a7d1ad376912ac75a6853485fd8e299754d9a
Albert Astals Cid <tsdgeos@terra.es>
2007-11-29 19:13:25 +0000

    You need >> or either the "Your names" and "Your messages" placeholders for translators are removed
    svn path=/trunk/KDE/kdebase/apps/; revision=743049

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` > rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT *.cpp -o $podir/dolphin.pot
 
