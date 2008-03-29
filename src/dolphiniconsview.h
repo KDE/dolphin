ae7d038d33590fb24d89b876e8a266d97357299c
Peter Penz <peter.penz19@gmail.com>
2008-03-29 19:40:26 +0000

    QListView might return a wrong x-position if the decoration size decreases. I temporary bypassed this in Dolphin, as I'm not 100 % sure whether it is really a QListView issue or related to KFileItemDelegate. To reproduce this issue with Qt-only quite a lot effort is needed -> I'll wait until Qt4.4 final is released...
    CCMAIL: fredrik@kde.org
    
    svn path=/trunk/KDE/kdebase/apps/; revision=791535

--- src/dolphiniconsview.h
+++ src/dolphiniconsview.h
@@ -47,6 +47,7 @@ class LIBDOLPHINPRIVATE_EXPORT DolphinIconsView : public KCategorizedView
 public:
     explicit DolphinIconsView(QWidget* parent, DolphinController* controller);
     virtual ~DolphinIconsView();
+    virtual QRect visualRect(const QModelIndex& index) const;
 
 protected:
     virtual QStyleOptionViewItem viewOptions() const;
