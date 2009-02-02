b1319c769cb69e118d4c99fc61363a73d1d6458c
Shaun Reich <shaun.reich@kdemail.net>
2009-02-02 20:51:15 +0000

    SVN_SILENT Fix to avoid accidental doxygen-highlighted header license.
    svn path=/trunk/KDE/kdebase/apps/; revision=920402

--- src/dolphincategorydrawer.h
+++ src/dolphincategorydrawer.h
@@ -1,5 +1,4 @@
-/**
-  * This file is part of the KDE project
+/* This file is part of the KDE project
   * Copyright (C) 2007 Rafael Fernández López <ereslibre@kde.org>
   *
   * This library is free software; you can redistribute it and/or
@@ -16,7 +15,7 @@
   * along with this library; see the file COPYING.LIB.  If not, write to
   * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
   * Boston, MA 02110-1301, USA.
-  */
+*/
 
 #ifndef DOLPHINCATEGORYDRAWER_H
 #define DOLPHINCATEGORYDRAWER_H
