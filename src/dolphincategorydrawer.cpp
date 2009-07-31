34b6b300506d2e8e842dcdf7de0325d5156c0ba4
Peter Penz <peter.penz19@gmail.com>
2009-07-31 15:29:37 +0000

    SVN_SILENT: warning--, fixed indentations
    svn path=/trunk/KDE/kdebase/apps/; revision=1005230

--- src/dolphincategorydrawer.cpp
+++ src/dolphincategorydrawer.cpp
@@ -43,7 +43,7 @@
 #define HORIZONTAL_HINT 3
 
 DolphinCategoryDrawer::DolphinCategoryDrawer()
-    : KCategoryDrawer()
+        : KCategoryDrawer()
 {
 }
 
@@ -54,86 +54,86 @@ DolphinCategoryDrawer::~DolphinCategoryDrawer()
 void DolphinCategoryDrawer::drawCategory(const QModelIndex &index, int sortRole,
                                          const QStyleOption &option, QPainter *painter) const
 {
-     painter->setRenderHint(QPainter::Antialiasing);
+    Q_UNUSED(sortRole);
+    painter->setRenderHint(QPainter::Antialiasing);
 
-     const QString category = index.model()->data(index, KCategorizedSortFilterProxyModel::CategoryDisplayRole).toString();
-     const QRect optRect = option.rect;
-     QFont font(QApplication::font());
-     font.setBold(true);
-     const QFontMetrics fontMetrics = QFontMetrics(font);
-     const int height = categoryHeight(index, option);
+    const QString category = index.model()->data(index, KCategorizedSortFilterProxyModel::CategoryDisplayRole).toString();
+    const QRect optRect = option.rect;
+    QFont font(QApplication::font());
+    font.setBold(true);
+    const QFontMetrics fontMetrics = QFontMetrics(font);
 
-     QColor outlineColor = option.palette.text().color();
-     outlineColor.setAlphaF(0.35);
+    QColor outlineColor = option.palette.text().color();
+    outlineColor.setAlphaF(0.35);
 
-     //BEGIN: top left corner
-     {
-         painter->save();
-         painter->setPen(outlineColor);
-         const QPointF topLeft(optRect.topLeft());
-         QRectF arc(topLeft, QSizeF(4, 4));
-         arc.translate(0.5, 0.5);
-         painter->drawArc(arc, 1440, 1440);
-         painter->restore();
-     }
-     //END: top left corner
+    //BEGIN: top left corner
+    {
+        painter->save();
+        painter->setPen(outlineColor);
+        const QPointF topLeft(optRect.topLeft());
+        QRectF arc(topLeft, QSizeF(4, 4));
+        arc.translate(0.5, 0.5);
+        painter->drawArc(arc, 1440, 1440);
+        painter->restore();
+    }
+    //END: top left corner
 
-     //BEGIN: left vertical line
-     {
-         QPoint start(optRect.topLeft());
-         start.ry() += 3;
-         QPoint verticalGradBottom(optRect.topLeft());
-         verticalGradBottom.ry() += fontMetrics.height() + 5;
-         QLinearGradient gradient(start, verticalGradBottom);
-         gradient.setColorAt(0, outlineColor);
-         gradient.setColorAt(1, Qt::transparent);
-         painter->fillRect(QRect(start, QSize(1, fontMetrics.height() + 5)), gradient);
-     }
-     //END: left vertical line
+    //BEGIN: left vertical line
+    {
+        QPoint start(optRect.topLeft());
+        start.ry() += 3;
+        QPoint verticalGradBottom(optRect.topLeft());
+        verticalGradBottom.ry() += fontMetrics.height() + 5;
+        QLinearGradient gradient(start, verticalGradBottom);
+        gradient.setColorAt(0, outlineColor);
+        gradient.setColorAt(1, Qt::transparent);
+        painter->fillRect(QRect(start, QSize(1, fontMetrics.height() + 5)), gradient);
+    }
+    //END: left vertical line
 
-     //BEGIN: horizontal line
-     {
-         QPoint start(optRect.topLeft());
-         start.rx() += 3;
-         QPoint horizontalGradTop(optRect.topLeft());
-         horizontalGradTop.rx() += optRect.width() - 6;
-         painter->fillRect(QRect(start, QSize(optRect.width() - 6, 1)), outlineColor);
-     }
-     //END: horizontal line
+    //BEGIN: horizontal line
+    {
+        QPoint start(optRect.topLeft());
+        start.rx() += 3;
+        QPoint horizontalGradTop(optRect.topLeft());
+        horizontalGradTop.rx() += optRect.width() - 6;
+        painter->fillRect(QRect(start, QSize(optRect.width() - 6, 1)), outlineColor);
+    }
+    //END: horizontal line
 
-     //BEGIN: top right corner
-     {
-         painter->save();
-         painter->setPen(outlineColor);
-         QPointF topRight(optRect.topRight());
-         topRight.rx() -= 4;
-         QRectF arc(topRight, QSizeF(4, 4));
-         arc.translate(0.5, 0.5);
-         painter->drawArc(arc, 0, 1440);
-         painter->restore();
-     }
-     //END: top right corner
+    //BEGIN: top right corner
+    {
+        painter->save();
+        painter->setPen(outlineColor);
+        QPointF topRight(optRect.topRight());
+        topRight.rx() -= 4;
+        QRectF arc(topRight, QSizeF(4, 4));
+        arc.translate(0.5, 0.5);
+        painter->drawArc(arc, 0, 1440);
+        painter->restore();
+    }
+    //END: top right corner
 
-     //BEGIN: right vertical line
-     {
-         QPoint start(optRect.topRight());
-         start.ry() += 3;
-         QPoint verticalGradBottom(optRect.topRight());
-         verticalGradBottom.ry() += fontMetrics.height() + 5;
-         QLinearGradient gradient(start, verticalGradBottom);
-         gradient.setColorAt(0, outlineColor);
-         gradient.setColorAt(1, Qt::transparent);
-         painter->fillRect(QRect(start, QSize(1, fontMetrics.height() + 5)), gradient);
-     }
-     //END: right vertical line
+    //BEGIN: right vertical line
+    {
+        QPoint start(optRect.topRight());
+        start.ry() += 3;
+        QPoint verticalGradBottom(optRect.topRight());
+        verticalGradBottom.ry() += fontMetrics.height() + 5;
+        QLinearGradient gradient(start, verticalGradBottom);
+        gradient.setColorAt(0, outlineColor);
+        gradient.setColorAt(1, Qt::transparent);
+        painter->fillRect(QRect(start, QSize(1, fontMetrics.height() + 5)), gradient);
+    }
+    //END: right vertical line
 
-     //BEGIN: category information
-     {
-         const int iconSize = KIconLoader::global()->currentSize(KIconLoader::Small);
+    //BEGIN: category information
+    {
+        const int iconSize = KIconLoader::global()->currentSize(KIconLoader::Small);
 
-         bool paintIcon;
-         QPixmap icon;
-         switch (index.column()) {
+        bool paintIcon;
+        QPixmap icon;
+        switch (index.column()) {
              case KDirModel::Owner: {
                      paintIcon = true;
                      KUser user(category);
@@ -159,37 +159,37 @@ void DolphinCategoryDrawer::drawCategory(const QModelIndex &index, int sortRole,
                  break;
              default:
                  paintIcon = false;
-         }
+             }
 
-         if (paintIcon) {
-             QRect iconRect(option.rect);
-             iconRect.setTop(iconRect.top() + 4);
-             iconRect.setLeft(iconRect.left() + 7);
-             iconRect.setSize(QSize(iconSize, iconSize));
+        if (paintIcon) {
+            QRect iconRect(option.rect);
+            iconRect.setTop(iconRect.top() + 4);
+            iconRect.setLeft(iconRect.left() + 7);
+            iconRect.setSize(QSize(iconSize, iconSize));
 
-             painter->drawPixmap(iconRect, icon);
-         }
+            painter->drawPixmap(iconRect, icon);
+        }
 
-         //BEGIN: text
-         {
-             QRect textRect(option.rect);
-             textRect.setTop(textRect.top() + 7);
-             textRect.setLeft(textRect.left() + 7 + (paintIcon ? (iconSize + 6) : 0));
-             textRect.setHeight(qMax(fontMetrics.height(), iconSize));
-             textRect.setRight(textRect.right() - 7);
-             textRect.setBottom(textRect.bottom() - 5); // only one pixel separation here (no gradient)
+        //BEGIN: text
+        {
+            QRect textRect(option.rect);
+            textRect.setTop(textRect.top() + 7);
+            textRect.setLeft(textRect.left() + 7 + (paintIcon ? (iconSize + 6) : 0));
+            textRect.setHeight(qMax(fontMetrics.height(), iconSize));
+            textRect.setRight(textRect.right() - 7);
+            textRect.setBottom(textRect.bottom() - 5); // only one pixel separation here (no gradient)
 
-             painter->save();
-             painter->setFont(font);
-             QColor penColor(option.palette.text().color());
-             penColor.setAlphaF(0.6);
-             painter->setPen(penColor);
-             painter->drawText(textRect, Qt::AlignLeft | Qt::AlignVCenter, category);
-             painter->restore();
-          }
-         //END: text
-     }
-     //BEGIN: category information
+            painter->save();
+            painter->setFont(font);
+            QColor penColor(option.palette.text().color());
+            penColor.setAlphaF(0.6);
+            painter->setPen(penColor);
+            painter->drawText(textRect, Qt::AlignLeft | Qt::AlignVCenter, category);
+            painter->restore();
+        }
+        //END: text
+    }
+    //BEGIN: category information
 }
 
 int DolphinCategoryDrawer::categoryHeight(const QModelIndex &index, const QStyleOption &option) const
@@ -199,17 +199,17 @@ int DolphinCategoryDrawer::categoryHeight(const QModelIndex &index, const QStyle
     bool paintIcon;
 
     switch (index.column()) {
-        case KDirModel::Owner:
-        case KDirModel::Type:
-            paintIcon = true;
-            break;
-        default:
-            paintIcon = false;
+    case KDirModel::Owner:
+    case KDirModel::Type:
+        paintIcon = true;
+        break;
+    default:
+        paintIcon = false;
     }
 
     if (paintIcon) {
         return qMax(heightWithoutIcon + 5, iconSize + 1 /* 1 pixel-width gradient */
-                                                    + 5 /* top and bottom separation */);
+                    + 5 /* top and bottom separation */);
     }
 
     return heightWithoutIcon + 5;
