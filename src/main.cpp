91b99e9fd339187f9f2cca3c32c02d93cfe5f474
Peter Penz <peter.penz19@gmail.com>
2011-01-01 18:02:34 +0000

    Increase version to 1.6.9
    svn path=/trunk/KDE/kdebase/apps/; revision=1210665

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ KDE_EXPORT int kdemain(int argc, char **argv)
 {
     KAboutData about("dolphin", 0,
                      ki18nc("@title", "Dolphin"),
-                     "1.5.9",
+                     "1.6.9",
                      ki18nc("@title", "File Manager"),
                      KAboutData::License_GPL,
                      ki18nc("@info:credit", "(C) 2006-2010 Peter Penz"));
