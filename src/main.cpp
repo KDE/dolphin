112c21a106cc5b235c17a3bd52431678cfe29e8a
Peter Penz <peter.penz19@gmail.com>
2011-01-30 21:04:24 +0100

    Updated version and copyright-information

--- src/main.cpp
+++ src/main.cpp
@@ -33,10 +33,10 @@ KDE_EXPORT int kdemain(int argc, char **argv)
 {
     KAboutData about("dolphin", 0,
                      ki18nc("@title", "Dolphin"),
-                     "1.6",
+                     "1.6.1",
                      ki18nc("@title", "File Manager"),
                      KAboutData::License_GPL,
-                     ki18nc("@info:credit", "(C) 2006-2010 Peter Penz"));
+                     ki18nc("@info:credit", "(C) 2006-2011 Peter Penz"));
     about.setHomepage("http://dolphin.kde.org");
     about.addAuthor(ki18nc("@info:credit", "Peter Penz"),
                     ki18nc("@info:credit", "Maintainer and developer"),
