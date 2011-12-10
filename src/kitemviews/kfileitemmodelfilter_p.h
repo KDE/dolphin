542a80874bcf8ca1345c63e3f34feb868ec368e0
Peter Penz <peter.penz19@gmail.com>
2011-12-10 21:37:40 +0100

    Minor fix in copyright header
    This class is not based on the ItemViews-NG project.

--- src/kitemviews/kfileitemmodelfilter_p.h
+++ src/kitemviews/kfileitemmodelfilter_p.h
@@ -2,9 +2,6 @@
  *   Copyright (C) 2011 by Janardhan Reddy                                 *
  *   <annapareddyjanardhanreddy@gmail.com>                                 *
  *                                                                         *
- *   Based on the Itemviews NG project from Trolltech Labs:                *
- *   http://qt.gitorious.org/qt-labs/itemviews-ng                          *
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
