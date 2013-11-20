32264f5546b7b1358572f06346b111f60e514d0a
Frank Reininghaus <frank78ac@googlemail.com>
2013-11-21 00:41:23 +0100

    Make the "Create New..." menu in the "item context menu" work again
    Since https://git.reviewboard.kde.org/r/111989/, the "Create New..."
    menu in the context menu that appears when right-clicking a folder was
    not a child of the DolphinMainWindow any more, but of the context menu
    itself. This is the reason why the dialog that asks for a file name
    when choosing one of the "Create New..." options disappeared
    immediately.
    
    This patch makes sure that the main window is the parent of the
    "Create New..." menu again.
    
    BUG: 327783
    REVIEW: 113930

--- src/dolphincontextmenu.cpp
+++ src/dolphincontextmenu.cpp
@@ -196,7 +196,7 @@ void DolphinContextMenu::openItemContextMenu()
     if (m_selectedItems.count() == 1) {
         if (m_fileInfo.isDir()) {
             // setup 'Create New' menu
-            DolphinNewFileMenu* newFileMenu = new DolphinNewFileMenu(m_mainWindow->actionCollection(), this);
+            DolphinNewFileMenu* newFileMenu = new DolphinNewFileMenu(m_mainWindow->actionCollection(), m_mainWindow);
             const DolphinView* view = m_mainWindow->activeViewContainer()->view();
             newFileMenu->setViewShowsHiddenFiles(view->hiddenFilesShown());
             newFileMenu->checkUpToDate();
