192f00e32f96ae45d1577744be1f7b86280969f8
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2014-12-14 18:39:08 +0100

    get rid of KFileItemActionPlugin

--- src/dolphincontextmenu.cpp
+++ src/dolphincontextmenu.cpp
@@ -27,8 +27,7 @@
 #include "dolphinremoveaction.h"
 
 #include <KActionCollection>
-#include <kfileitemactionplugin.h>
-#include <kabstractfileitemactionplugin.h>
+#include <KAbstractFileItemActionPlugin>
 #include <KFileItemActions>
 #include <KFileItemListProperties>
 #include <KIO/RestoreJob>
@@ -512,13 +511,6 @@ void DolphinContextMenu::addFileItemPluginActions()
             continue;
         }
 
-        // Old API (kdelibs-4.6.0 only)
-        KFileItemActionPlugin* plugin = service->createInstance<KFileItemActionPlugin>();
-        if (plugin) {
-            plugin->setParent(this);
-            addActions(plugin->actions(props, m_mainWindow));
-        }
-        // New API (kdelibs >= 4.6.1)
         KAbstractFileItemActionPlugin* abstractPlugin = service->createInstance<KAbstractFileItemActionPlugin>();
         if (abstractPlugin) {
             abstractPlugin->setParent(this);
