a8f4f01eaa4027476ea799b7815f353f5cdd99ad
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-02-19 20:09:47 +0100

    Increase the version number to 14.12.3

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ KDE_EXPORT int kdemain(int argc, char **argv)
 {
     KAboutData about("dolphin", 0,
                      ki18nc("@title", "Dolphin"),
-                     "14.11.97",
+                     "14.12.3",
                      ki18nc("@title", "File Manager"),
                      KAboutData::License_GPL,
                      ki18nc("@info:credit", "(C) 2006-2014 Peter Penz, Frank Reininghaus, and Emmanuel Pescosta"));
