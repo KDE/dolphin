2e8e30026a207d380379fb2be0e4327bf4f455e8
Marc André Wittorf <marc-a@gmx.de>
2017-01-21 01:37:35 +0100

    Fix missing audio duration in details view
    Enabling the 'duration' column in a folder with audio files did only show empty information. Properly initializing the QTime object fixes this.
    
     Testing Done
    
        Find a folder with audio files
        Make sure that Baloo has indexed this folder
        Open this folder in Dolphin, detail view, enable the Audio/Duration column
        Unpatched Dolphin does not display audio duration, patched Dolphin does
    
    Tested on Gentoo x86_64 with Qt 5.5.1, KF 5.21.0 and Arch x86_64 with Qt 5.6.0 and KF 5.21.0.
    
    REVIEW: 127799

--- src/kitemviews/private/kbaloorolesprovider.cpp
+++ src/kitemviews/private/kbaloorolesprovider.cpp
@@ -181,7 +181,7 @@ QString KBalooRolesProvider::orientationFromValue(int value) const
 
 QString KBalooRolesProvider::durationFromValue(int value) const
 {
-    QTime duration;
+    QTime duration(0, 0, 0);
     duration = duration.addSecs(value);
     return duration.toString(QStringLiteral("hh:mm:ss"));
 }
