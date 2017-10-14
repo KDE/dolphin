KDE/dolphin

commit bd47eb2e6d80315115bb3f58987162fcb4911b10

Don't block unmounting when terminal panel's cwd is the mountpoint
When unmounting a removable media Dolphin checks if there are
some files open on the device before performing the unmount.
If the terminal window in dolphin is open and the to be unmounted path is open,
 the unmount process will be blocked.

This patch sets the terminal window current path to the home directory upon unmount request
 if the terminal directory is set to the mount path.

The unmount request could came from two sources:

    The user could hit right click on the media in the dolphin's places panel and hit unmount.
    The user could request an unmount from the indicator applet

This patch was originally written by Arjun AK for the kdelibs4 version
of Dolphin: https://git.reviewboard.kde.org/r/121613/

BUG: 158264
FIXED-IN: 17.11.80

Differential Revision: https://phabricator.kde.org/D7847

src/dolphinmainwindow.cpp
@@ -99,7 +99,10 @@ DolphinMainWindow::DolphinMainWindow() :
     m_settingsDialog(),
     m_controlButton(0),
     m_updateToolBarTimer(0),
-    m_lastHandleUrlStatJob(0)
+    m_lastHandleUrlStatJob(0),
+    m_terminalPanel(0),
+    m_placesPanel(0),
+    m_tearDownFromPlacesRequested(false)
 {
     Q_INIT_RESOURCE(dolphin);
 
@@ -247,6 +250,11 @@ void DolphinMainWindow::changeUrl(const QUrl &url)
 
 void DolphinMainWindow::slotTerminalDirectoryChanged(const QUrl& url)
 {
+    if (m_tearDownFromPlacesRequested && url == QUrl::fromLocalFile(QDir::homePath())) {
+        m_placesPanel->proceedWithTearDown();
+        m_tearDownFromPlacesRequested = false;
+    }
+
     m_activeViewContainer->setAutoGrabFocus(false);
     changeUrl(url);
     m_activeViewContainer->setAutoGrabFocus(true);
@@ -994,6 +1002,25 @@ void DolphinMainWindow::setUrlAsCaption(const QUrl& url)
     setWindowTitle(schemePrefix + fileName);
 }
 
+void DolphinMainWindow::slotStorageTearDownFromPlacesRequested(const QString& mountPath)
+{
+    if (m_terminalPanel->currentWorkingDirectory().startsWith(mountPath)) {
+        m_tearDownFromPlacesRequested = true;
+        m_terminalPanel->goHome();
+        // m_placesPanel->proceedWithTearDown() will be called in slotTerminalDirectoryChanged
+    } else {
+        m_placesPanel->proceedWithTearDown();
+    }
+}
+
+void DolphinMainWindow::slotStorageTearDownExternallyRequested(const QString& mountPath)
+{
+    if (m_terminalPanel->currentWorkingDirectory().startsWith(mountPath)) {
+        m_tearDownFromPlacesRequested = false;
+        m_terminalPanel->goHome();
+    }
+}
+
 void DolphinMainWindow::setupActions()
 {
     // setup 'File' menu
@@ -1246,21 +1273,21 @@ void DolphinMainWindow::setupDockWidgets()
         terminalDock->setLocked(lock);
         terminalDock->setObjectName(QStringLiteral("terminalDock"));
         terminalDock->setAllowedAreas(Qt::TopDockWidgetArea | Qt::BottomDockWidgetArea);
-        TerminalPanel* terminalPanel = new TerminalPanel(terminalDock);
-        terminalPanel->setCustomContextMenuActions({lockLayoutAction});
-        terminalDock->setWidget(terminalPanel);
+        m_terminalPanel = new TerminalPanel(terminalDock);
+        m_terminalPanel->setCustomContextMenuActions({lockLayoutAction});
+        terminalDock->setWidget(m_terminalPanel);
 
-        connect(terminalPanel, &TerminalPanel::hideTerminalPanel, terminalDock, &DolphinDockWidget::hide);
-        connect(terminalPanel, &TerminalPanel::changeUrl, this, &DolphinMainWindow::slotTerminalDirectoryChanged);
+        connect(m_terminalPanel, &TerminalPanel::hideTerminalPanel, terminalDock, &DolphinDockWidget::hide);
+        connect(m_terminalPanel, &TerminalPanel::changeUrl, this, &DolphinMainWindow::slotTerminalDirectoryChanged);
         connect(terminalDock, &DolphinDockWidget::visibilityChanged,
-                terminalPanel, &TerminalPanel::dockVisibilityChanged);
+                m_terminalPanel, &TerminalPanel::dockVisibilityChanged);
 
         QAction* terminalAction = terminalDock->toggleViewAction();
         createPanelAction(QIcon::fromTheme(QStringLiteral("utilities-terminal")), Qt::Key_F4, terminalAction, QStringLiteral("show_terminal_panel"));
 
         addDockWidget(Qt::BottomDockWidgetArea, terminalDock);
         connect(this, &DolphinMainWindow::urlChanged,
-                terminalPanel, &TerminalPanel::setUrl);
+                m_terminalPanel, &TerminalPanel::setUrl);
 
         if (GeneralSettings::version() < 200) {
             terminalDock->hide();
@@ -1279,28 +1306,31 @@ void DolphinMainWindow::setupDockWidgets()
     placesDock->setObjectName(QStringLiteral("placesDock"));
     placesDock->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
 
-    PlacesPanel* placesPanel = new PlacesPanel(placesDock);
-    placesPanel->setCustomContextMenuActions({lockLayoutAction});
-    placesDock->setWidget(placesPanel);
+    m_placesPanel = new PlacesPanel(placesDock);
+    m_placesPanel->setCustomContextMenuActions({lockLayoutAction});
+    placesDock->setWidget(m_placesPanel);
 
-    QAction* placesAction = placesDock->toggleViewAction();
+    QAction *placesAction = placesDock->toggleViewAction();
     createPanelAction(QIcon::fromTheme(QStringLiteral("bookmarks")), Qt::Key_F9, placesAction, QStringLiteral("show_places_panel"));
 
     addDockWidget(Qt::LeftDockWidgetArea, placesDock);
-    connect(placesPanel, &PlacesPanel::placeActivated,
+    connect(m_placesPanel, &PlacesPanel::placeActivated,
             this, &DolphinMainWindow::slotPlaceActivated);
-    connect(placesPanel, &PlacesPanel::placeMiddleClicked,
+    connect(m_placesPanel, &PlacesPanel::placeMiddleClicked,
             this, &DolphinMainWindow::openNewTab);
-    connect(placesPanel, &PlacesPanel::errorMessage,
+    connect(m_placesPanel, &PlacesPanel::errorMessage,
             this, &DolphinMainWindow::showErrorMessage);
     connect(this, &DolphinMainWindow::urlChanged,
-            placesPanel, &PlacesPanel::setUrl);
+            m_placesPanel, &PlacesPanel::setUrl);
     connect(placesDock, &DolphinDockWidget::visibilityChanged,
             m_tabWidget, &DolphinTabWidget::slotPlacesPanelVisibilityChanged);
     connect(this, &DolphinMainWindow::settingsChanged,
-	    placesPanel, &PlacesPanel::readSettings);
-
-    m_tabWidget->slotPlacesPanelVisibilityChanged(placesPanel->isVisible());
+        m_placesPanel, &PlacesPanel::readSettings);
+    connect(m_placesPanel, &PlacesPanel::storageTearDownRequested,
+            this, &DolphinMainWindow::slotStorageTearDownFromPlacesRequested);
+    connect(m_placesPanel, &PlacesPanel::storageTearDownExternallyRequested,
+            this, &DolphinMainWindow::slotStorageTearDownExternallyRequested);
+    m_tabWidget->slotPlacesPanelVisibilityChanged(m_placesPanel->isVisible());
 
     // Add actions into the "Panels" menu
     KActionMenu* panelsMenu = new KActionMenu(i18nc("@action:inmenu View", "Panels"), this);

src/dolphinmainwindow.h
@@ -46,6 +46,8 @@ class KJob;
 class KNewFileMenu;
 class QToolButton;
 class QIcon;
+class PlacesPanel;
+class TerminalPanel;
 
 /**
  * @short Main window for Dolphin.
@@ -89,6 +91,8 @@ public:
      * with other menus (e. g. a context menu).
      */
     KNewFileMenu* newFileMenu() const;
+
+    void setTabsToHomeIfMountPathOpen(const QString& mountPath);
 
 public slots:
     /**
@@ -410,6 +414,21 @@ private slots:
     void setUrlAsCaption(const QUrl& url);
 
     /**
+     * This slot is called when the user requested to unmount a removable media
+     * from the places menu
+     */
+    void slotStorageTearDownFromPlacesRequested(const QString& mountPath);
+
+    /**
+     * This slot is called when the user requested to unmount a removable media
+     * _not_ from the dolphin's places menu (from the notification area for e.g.)
+     * This slot is basically connected to each removable device's
+     * Solid::StorageAccess::teardownRequested(const QString & udi)
+     * signal through the places panel.
+     */
+    void slotStorageTearDownExternallyRequested(const QString& mountPath);
+
+    /**
      * Is called when the view has finished loading the directory.
      */
     void slotDirectoryLoadingCompleted();
@@ -496,6 +515,10 @@ private:
     QTimer* m_updateToolBarTimer;
 
     KIO::Job* m_lastHandleUrlStatJob;
+
+    TerminalPanel* m_terminalPanel;
+    PlacesPanel* m_placesPanel;
+    bool m_tearDownFromPlacesRequested;
 };
 
 inline DolphinViewContainer* DolphinMainWindow::activeViewContainer() const

src/panels/places/placesitem.cpp
@@ -272,6 +272,8 @@ void PlacesItem::initializeDevice(const QString& udi)
         setUrl(QUrl::fromLocalFile(m_access->filePath()));
         QObject::connect(m_access.data(), &Solid::StorageAccess::accessibilityChanged,
                          m_signalHandler.data(), &PlacesItemSignalHandler::onAccessibilityChanged);
+        QObject::connect(m_access.data(), &Solid::StorageAccess::teardownRequested,
+                         m_signalHandler.data(), &PlacesItemSignalHandler::onTearDownRequested);
     } else if (m_disc && (m_disc->availableContent() & Solid::OpticalDisc::Audio) != 0) {
         Solid::Block *block = m_device.as<Solid::Block>();
         if (block) {
@@ -334,4 +336,9 @@ QString PlacesItem::generateNewId()
     static int count = 0;
     return QString::number(QDateTime::currentDateTimeUtc().toTime_t()) +
             '/' + QString::number(count++) + " (V2)";
+}
+
+PlacesItemSignalHandler *PlacesItem::signalHandler() const
+{
+    return m_signalHandler.data();
 }

src/panels/places/placesitem.h
@@ -79,6 +79,8 @@ public:
     static KBookmark createDeviceBookmark(KBookmarkManager* manager,
                                           const QString& udi);
 
+    PlacesItemSignalHandler* signalHandler() const;
+
 protected:
     virtual void onDataValueChanged(const QByteArray& role,
                                     const QVariant& current,

src/panels/places/placesitemmodel.cpp
@@ -22,6 +22,7 @@
  ***************************************************************************/
 
 #include "placesitemmodel.h"
+#include "placesitemsignalhandler.h"
 
 #include "dolphin_generalsettings.h"
 
@@ -77,6 +78,7 @@ PlacesItemModel::PlacesItemModel(QObject* parent) :
     m_systemBookmarksIndexes(),
     m_bookmarkedItems(),
     m_hiddenItemToRemove(-1),
+    m_deviceToTearDown(0),
     m_updateBookmarksTimer(0),
     m_storageSetupInProgress()
 {
@@ -306,7 +308,7 @@ void PlacesItemModel::requestEject(int index)
         Solid::OpticalDrive* drive = item->device().parent().as<Solid::OpticalDrive>();
         if (drive) {
             connect(drive, &Solid::OpticalDrive::ejectDone,
-                    this, &PlacesItemModel::slotStorageTeardownDone);
+                    this, &PlacesItemModel::slotStorageTearDownDone);
             drive->eject();
         } else {
             const QString label = item->text();
@@ -316,15 +318,19 @@ void PlacesItemModel::requestEject(int index)
     }
 }
 
-void PlacesItemModel::requestTeardown(int index)
+void PlacesItemModel::requestTearDown(int index)
 {
     const PlacesItem* item = placesItem(index);
     if (item) {
-        Solid::StorageAccess* access = item->device().as<Solid::StorageAccess>();
-        if (access) {
-            connect(access, &Solid::StorageAccess::teardownDone,
-                    this, &PlacesItemModel::slotStorageTeardownDone);
-            access->teardown();
+        Solid::StorageAccess *tmp = item->device().as<Solid::StorageAccess>();
+        if (tmp) {
+            m_deviceToTearDown = tmp;
+            // disconnect the Solid::StorageAccess::teardownRequested
+            // to prevent emitting PlacesItemModel::storageTearDownExternallyRequested
+            // after we have emitted PlacesItemModel::storageTearDownRequested
+            disconnect(tmp, &Solid::StorageAccess::teardownRequested,
+                       item->signalHandler(), &PlacesItemSignalHandler::onTearDownRequested);
+            emit storageTearDownRequested(tmp->filePath());
         }
     }
 }
@@ -550,7 +556,11 @@ void PlacesItemModel::slotDeviceAdded(const QString& udi)
 
     m_availableDevices << udi;
     const KBookmark bookmark = PlacesItem::createDeviceBookmark(m_bookmarkManager, udi);
-    appendItem(new PlacesItem(bookmark));
+
+    PlacesItem *item = new PlacesItem(bookmark);
+    appendItem(item);
+    connect(item->signalHandler(), &PlacesItemSignalHandler::tearDownExternallyRequested,
+            this, &PlacesItemModel::storageTearDownExternallyRequested);
 }
 
 void PlacesItemModel::slotDeviceRemoved(const QString& udi)
@@ -576,11 +586,13 @@ void PlacesItemModel::slotDeviceRemoved(const QString& udi)
     }
 }
 
-void PlacesItemModel::slotStorageTeardownDone(Solid::ErrorType error, const QVariant& errorData)
+void PlacesItemModel::slotStorageTearDownDone(Solid::ErrorType error, const QVariant& errorData)
 {
     if (error && errorData.isValid()) {
         emit errorMessage(errorData.toString());
     }
+    m_deviceToTearDown->disconnect();
+    m_deviceToTearDown = nullptr;
 }
 
 void PlacesItemModel::slotStorageSetupDone(Solid::ErrorType error,
@@ -786,7 +798,10 @@ void PlacesItemModel::loadBookmarks()
     devicesItems.reserve(devicesItems.count() + devices.count());
     foreach (const QString& udi, devices) {
         const KBookmark bookmark = PlacesItem::createDeviceBookmark(m_bookmarkManager, udi);
-        devicesItems.append(new PlacesItem(bookmark));
+        PlacesItem *item = new PlacesItem(bookmark);
+        devicesItems.append(item);
+        connect(item->signalHandler(), &PlacesItemSignalHandler::tearDownExternallyRequested,
+                this, &PlacesItemModel::storageTearDownExternallyRequested);
     }
 
     QList<PlacesItem*> items;
@@ -938,6 +953,15 @@ void PlacesItemModel::createSystemBookmarks()
 void PlacesItemModel::clear() {
     m_bookmarkedItems.clear();
     KStandardItemModel::clear();
+}
+
+void PlacesItemModel::proceedWithTearDown()
+{
+    Q_ASSERT(m_deviceToTearDown);
+
+    connect(m_deviceToTearDown, &Solid::StorageAccess::teardownDone,
+            this, &PlacesItemModel::slotStorageTearDownDone);
+    m_deviceToTearDown->teardown();
 }
 
 void PlacesItemModel::initializeAvailableDevices()

src/panels/places/placesitemmodel.h
@@ -102,7 +102,7 @@ public:
     QAction* teardownAction(int index) const;
 
     void requestEject(int index);
-    void requestTeardown(int index);
+    void requestTearDown(int index);
 
     bool storageSetupNeeded(int index) const;
     void requestStorageSetup(int index);
@@ -123,6 +123,8 @@ public:
 
     virtual void clear() Q_DECL_OVERRIDE;
 
+    void proceedWithTearDown();
+
     /**
      * Saves the bookmarks and indicates to other applications that the
      * state of the bookmarks has been changed. Is only called by the
@@ -133,6 +135,8 @@ public:
 signals:
     void errorMessage(const QString& message);
     void storageSetupDone(int index, bool success);
+    void storageTearDownRequested(const QString& mountPath);
+    void storageTearDownExternallyRequested(const QString& mountPath);
 
 protected:
     virtual void onItemInserted(int index) Q_DECL_OVERRIDE;
@@ -142,7 +146,7 @@ protected:
 private slots:
     void slotDeviceAdded(const QString& udi);
     void slotDeviceRemoved(const QString& udi);
-    void slotStorageTeardownDone(Solid::ErrorType error, const QVariant& errorData);
+    void slotStorageTearDownDone(Solid::ErrorType error, const QVariant& errorData);
     void slotStorageSetupDone(Solid::ErrorType error, const QVariant& errorData, const QString& udi);
     void hideItem();
 
@@ -280,6 +284,8 @@ private:
     // asynchronously as in the scope of onItemChanged()
     // removing an item is not allowed.
     int m_hiddenItemToRemove;
+
+    Solid::StorageAccess *m_deviceToTearDown;
 
     QTimer* m_updateBookmarksTimer;
 

src/panels/places/placesitemsignalhandler.cpp
@@ -47,3 +47,15 @@ void PlacesItemSignalHandler::onTrashDirListerCompleted()
     }
 }
 
+void PlacesItemSignalHandler::onTearDownRequested(const QString& udi)
+{
+    Q_UNUSED(udi)
+    if (m_item) {
+        Solid::StorageAccess *tmp = m_item->device().as<Solid::StorageAccess>();
+        if (tmp) {
+            QString mountPath = tmp->filePath();
+            emit tearDownExternallyRequested(mountPath);
+        }
+    }
+}
+

src/panels/places/placesitemsignalhandler.h
@@ -61,6 +61,11 @@ public slots:
      */
     void onTrashDirListerCompleted();
 
+    void onTearDownRequested(const QString& udi);
+
+signals:
+    void tearDownExternallyRequested(const QString& udi);
+
 private:
     PlacesItem* m_item;
 };

commit 7787f080678ffd428b269acd8db370a8c0026b37

Fix build on Windows
TerminalPanel is not available on Windows, so commit bd47eb2e6d broke
the MSVC build on the CI.

src/dolphinmainwindow.cpp
@@ -100,7 +100,9 @@ DolphinMainWindow::DolphinMainWindow() :
     m_controlButton(0),
     m_updateToolBarTimer(0),
     m_lastHandleUrlStatJob(0),
+#ifndef Q_OS_WIN
     m_terminalPanel(0),
+#endif
     m_placesPanel(0),
     m_tearDownFromPlacesRequested(false)
 {
@@ -1002,6 +1004,7 @@ void DolphinMainWindow::setUrlAsCaption(const QUrl& url)
     setWindowTitle(schemePrefix + fileName);
 }
 
+#ifndef Q_OS_WIN
 void DolphinMainWindow::slotStorageTearDownFromPlacesRequested(const QString& mountPath)
 {
     if (m_terminalPanel->currentWorkingDirectory().startsWith(mountPath)) {
@@ -1020,6 +1023,7 @@ void DolphinMainWindow::slotStorageTearDownExternallyRequested(const QString& mo
         m_terminalPanel->goHome();
     }
 }
+#endif
 
 void DolphinMainWindow::setupActions()
 {

src/dolphinmainwindow.h
@@ -47,7 +47,9 @@ class KNewFileMenu;
 class QToolButton;
 class QIcon;
 class PlacesPanel;
+#ifndef Q_OS_WIN
 class TerminalPanel;
+#endif
 
 /**
  * @short Main window for Dolphin.
@@ -516,7 +518,9 @@ private:
 
     KIO::Job* m_lastHandleUrlStatJob;
 
+#ifndef Q_OS_WIN
     TerminalPanel* m_terminalPanel;
+#endif
     PlacesPanel* m_placesPanel;
     bool m_tearDownFromPlacesRequested;
 };