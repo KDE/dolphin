d9956c620eb38baa6e534eb14a8d376ea07ed8c6
Elvis Angelaccio <elvis.angelaccio@kde.org>
2020-10-14 23:53:00 +0200

    Drop obsolete version check
    We require Qt 5.14 now.

--- src/kitemviews/private/kitemlistsmoothscroller.cpp
+++ src/kitemviews/private/kitemlistsmoothscroller.cpp
@@ -21,13 +21,8 @@ KItemListSmoothScroller::KItemListSmoothScroller(QScrollBar* scrollBar,
     m_animation(nullptr)
 {
     m_animation = new QPropertyAnimation(this);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
     const int animationDuration = m_scrollBar->style()->styleHint(QStyle::SH_Widget_Animation_Duration, nullptr, m_scrollBar);
     const bool animationEnabled = (animationDuration > 0);
- #else
-    const int animationDuration = 100;
-    const bool animationEnabled = m_scrollBar->style()->styleHint(QStyle::SH_Widget_Animate, nullptr, m_scrollBar);
-#endif
     m_animation->setDuration(animationEnabled ? animationDuration : 1);
     connect(m_animation, &QPropertyAnimation::stateChanged,
             this, &KItemListSmoothScroller::slotAnimationStateChanged);
