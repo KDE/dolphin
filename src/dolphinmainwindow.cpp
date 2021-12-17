b2c137fdb7e552131c5a31db657f395f2f09e672
Kai Uwe Broulik <kde@privat.broulik.de>
2021-12-17 22:43:15 +0100

    Use QUrl::toDisplayString when presenting to the user
    Strips out password information

--- src/dolphinmainwindow.cpp
+++ src/dolphinmainwindow.cpp
@@ -750,7 +750,7 @@ void DolphinMainWindow::slotAboutToShowBackPopupMenu()
     int entries = 0;
     m_backAction->menu()->clear();
     for (int i = urlNavigator->historyIndex() + 1; i < urlNavigator->historySize() && entries < MaxNumberOfNavigationentries; ++i, ++entries) {
-        QAction* action = new QAction(urlNavigator->locationUrl(i).toString(QUrl::PreferLocalFile), m_backAction->menu());
+        QAction* action = new QAction(urlNavigator->locationUrl(i).toDisplayString(QUrl::PreferLocalFile), m_backAction->menu());
         action->setData(i);
         m_backAction->menu()->addAction(action);
     }
@@ -779,7 +779,7 @@ void DolphinMainWindow::slotAboutToShowForwardPopupMenu()
     int entries = 0;
     m_forwardAction->menu()->clear();
     for (int i = urlNavigator->historyIndex() - 1; i >= 0 && entries < MaxNumberOfNavigationentries; --i, ++entries) {
-        QAction* action = new QAction(urlNavigator->locationUrl(i).toString(QUrl::PreferLocalFile), m_forwardAction->menu());
+        QAction* action = new QAction(urlNavigator->locationUrl(i).toDisplayString(QUrl::PreferLocalFile), m_forwardAction->menu());
         action->setData(i);
         m_forwardAction->menu()->addAction(action);
     }
