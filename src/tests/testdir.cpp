2f62b054eabffb657976f94abd49c6445878b647
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-12-19 14:10:13 +0100

    GIT_SILENT Port remaining QDateTime::toTime_t usage in windows code path

--- src/tests/testdir.cpp
+++ src/tests/testdir.cpp
@@ -36,7 +36,7 @@ static void setTimeStamp(const QString& path, const QDateTime& mtime)
     utime(QFile::encodeName(path), &utbuf);
 #elif defined(Q_OS_WIN)
     struct _utimbuf utbuf;
-    utbuf.actime = mtime.toTime_t();
+    utbuf.actime = mtime.toSecsSinceEpoch();
     utbuf.modtime = utbuf.actime;
     _wutime(reinterpret_cast<const wchar_t *>(path.utf16()), &utbuf);
 #endif
