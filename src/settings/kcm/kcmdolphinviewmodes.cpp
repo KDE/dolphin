KDE/dolphin

commit 489b56b68bb29e81337e115c490eea4403001b71

Dolphin settings revamp
It includes a move of the settings in the Navigation and Startup sections to the Interface (formerly Behavior) section.
It also includes a new tab in the View (formerly View Mode) section, called General where some settings regarding Display style, Browsing and Miscellaneous settings
The Interface section has new tabs named Folders & Tabs and Status & Location bars respectively where most of the Startup and Navigation settings moved.

The `dolphin/kcms/kcm_dolphinnavigation` kcm is removed.

src/CMakeLists.txt
@@ -288,25 +288,24 @@ target_sources(dolphinstatic PRIVATE
     selectionmode/bottombar.cpp
     selectionmode/bottombarcontentscontainer.cpp
     selectionmode/topbar.cpp
-    settings/general/behaviorsettingspage.cpp
-    settings/general/configurepreviewplugindialog.cpp
-    settings/general/confirmationssettingspage.cpp
-    settings/general/generalsettingspage.cpp
-    settings/general/previewssettingspage.cpp
-    settings/general/statusbarsettingspage.cpp
+    settings/interface/folderstabssettingspage.cpp
+    settings/interface/statusandlocationbarssettingspage.cpp
+    settings/interface/configurepreviewplugindialog.cpp
+    settings/interface/confirmationssettingspage.cpp
+    settings/interface/interfacesettingspage.cpp
+    settings/interface/previewssettingspage.cpp
     settings/dolphinsettingsdialog.cpp
-    settings/navigation/navigationsettingspage.cpp
     settings/contextmenu/contextmenusettingspage.cpp
     settings/settingspagebase.cpp
     settings/serviceitemdelegate.cpp
     settings/servicemodel.cpp
-    settings/startup/startupsettingspage.cpp
     settings/trash/trashsettingspage.cpp
     settings/viewmodes/dolphinfontrequester.cpp
     settings/viewmodes/viewsettingspage.cpp
     settings/viewmodes/viewmodesettings.cpp
     settings/viewmodes/viewsettingstab.cpp
     settings/viewmodes/contentdisplaytab.cpp
+    settings/viewmodes/generalviewsettingspage.cpp
     statusbar/dolphinstatusbar.cpp
     statusbar/mountpointobserver.cpp
     statusbar/mountpointobservercache.cpp
@@ -348,25 +347,24 @@ target_sources(dolphinstatic PRIVATE
     selectionmode/bottombar.h
     selectionmode/bottombarcontentscontainer.h
     selectionmode/topbar.h
-    settings/general/behaviorsettingspage.h
-    settings/general/configurepreviewplugindialog.h
-    settings/general/confirmationssettingspage.h
-    settings/general/generalsettingspage.h
-    settings/general/previewssettingspage.h
-    settings/general/statusbarsettingspage.h
+    settings/interface/folderstabssettingspage.h
+    settings/interface/statusandlocationbarssettingspage.h
+    settings/interface/configurepreviewplugindialog.h
+    settings/interface/confirmationssettingspage.h
+    settings/interface/interfacesettingspage.h
+    settings/interface/previewssettingspage.h
     settings/dolphinsettingsdialog.h
-    settings/navigation/navigationsettingspage.h
     settings/contextmenu/contextmenusettingspage.h
     settings/settingspagebase.h
     settings/serviceitemdelegate.h
     settings/servicemodel.h
-    settings/startup/startupsettingspage.h
     settings/trash/trashsettingspage.h
     settings/viewmodes/dolphinfontrequester.h
     settings/viewmodes/viewsettingspage.h
     settings/viewmodes/viewmodesettings.h
     settings/viewmodes/viewsettingstab.h
     settings/viewmodes/contentdisplaytab.h
+    settings/viewmodes/generalviewsettingspage.h
     statusbar/dolphinstatusbar.h
     statusbar/mountpointobserver.h
     statusbar/mountpointobservercache.h
@@ -500,7 +498,6 @@ install(TARGETS dolphin ${KDE_INSTALL_TARGETS_DEFAULT_ARGS})
 if(NOT WIN32)
     # The settings are still accessible from the hamburger menu
     add_library(kcm_dolphinviewmodes MODULE)
-    add_library(kcm_dolphinnavigation MODULE)
     add_library(kcm_dolphingeneral MODULE)
 
     target_sources(kcm_dolphinviewmodes PRIVATE
@@ -515,32 +512,37 @@ if(NOT WIN32)
         settings/viewmodes/viewsettingstab.h
         views/zoomlevelinfo.h)
 
-    target_sources(kcm_dolphinnavigation PRIVATE
-        settings/kcm/kcmdolphinnavigation.cpp
-        settings/navigation/navigationsettingspage.cpp
-        settings/settingspagebase.cpp
-        settings/kcm/kcmdolphinnavigation.h
-        settings/navigation/navigationsettingspage.h
-        settings/settingspagebase.h
-    )
-
     target_sources(kcm_dolphingeneral PRIVATE
         settings/kcm/kcmdolphingeneral.cpp
-        settings/general/behaviorsettingspage.cpp
-        settings/general/previewssettingspage.cpp
-        settings/general/configurepreviewplugindialog.cpp
-        settings/general/confirmationssettingspage.cpp
+        settings/interface/folderstabssettingspage.cpp
+        settings/interface/statusandlocationbarssettingspage.cpp
+        settings/interface/previewssettingspage.cpp
+        settings/interface/configurepreviewplugindialog.cpp
+        settings/interface/confirmationssettingspage.cpp
         settings/settingspagebase.cpp
         settings/serviceitemdelegate.cpp
         settings/servicemodel.cpp
+        dolphin_generalsettings.cpp
+        dolphindebug.cpp
+        dolphinmainwindowinterface.cpp
+        views/viewproperties.cpp
+        dolphin_directoryviewpropertysettings.cpp
+        global.cpp
         settings/kcm/kcmdolphingeneral.h
-        settings/general/behaviorsettingspage.h
-        settings/general/previewssettingspage.h
-        settings/general/configurepreviewplugindialog.h
-        settings/general/confirmationssettingspage.h
+        settings/interface/folderstabssettingspage.h
+        settings/interface/statusandlocationbarssettingspage.h
+        settings/interface/previewssettingspage.h
+        settings/interface/configurepreviewplugindialog.h
+        settings/interface/confirmationssettingspage.h
         settings/settingspagebase.h
         settings/serviceitemdelegate.h
         settings/servicemodel.h
+        dolphin_generalsettings.h
+        dolphindebug.h
+        dolphinmainwindowinterface.h
+        views/viewproperties.h
+        dolphin_directoryviewpropertysettings.h
+        global.h
     )
 
     kconfig_add_kcfg_files(kcm_dolphinviewmodes
@@ -553,20 +555,15 @@ if(NOT WIN32)
         settings/dolphin_versioncontrolsettings.kcfgc
     )
 
-    kconfig_add_kcfg_files(kcm_dolphinnavigation
-        settings/dolphin_generalsettings.kcfgc)
-
     kconfig_add_kcfg_files(kcm_dolphingeneral
         settings/dolphin_generalsettings.kcfgc)
 
     target_link_libraries(kcm_dolphinviewmodes dolphinprivate)
-    target_link_libraries(kcm_dolphinnavigation dolphinprivate)
     target_link_libraries(kcm_dolphingeneral dolphinprivate)
 
     install( FILES org.kde.dolphin.appdata.xml DESTINATION ${KDE_INSTALL_METAINFODIR} )
 
     install(TARGETS kcm_dolphinviewmodes DESTINATION ${KDE_INSTALL_PLUGINDIR}/dolphin/kcms )
-    install(TARGETS kcm_dolphinnavigation DESTINATION ${KDE_INSTALL_PLUGINDIR}/dolphin/kcms )
     install(TARGETS kcm_dolphingeneral DESTINATION ${KDE_INSTALL_PLUGINDIR}/dolphin/kcms )
 endif()
 

src/settings/viewmodes/viewsettingspage.cpp
@@ -7,14 +7,14 @@
 #include "viewsettingspage.h"
 
 #include "contentdisplaytab.h"
+#include "generalviewsettingspage.h"
 #include "viewsettingstab.h"
-
 #include <KLocalizedString>
 
 #include <QTabWidget>
 #include <QVBoxLayout>
 
-ViewSettingsPage::ViewSettingsPage(QWidget *parent)
+ViewSettingsPage::ViewSettingsPage(const QUrl &url, QWidget *parent)
     : SettingsPageBase(parent)
     , m_tabs()
 {
@@ -22,6 +22,11 @@ ViewSettingsPage::ViewSettingsPage(QWidget *parent)
     topLayout->setContentsMargins(0, 0, 0, 0);
 
     tabWidget = new QTabWidget(this);
+
+    // General View tab
+    GeneralViewSettingsPage *generalViewPage = new GeneralViewSettingsPage(url, tabWidget);
+    tabWidget->addTab(generalViewPage, QIcon::fromTheme(QStringLiteral("description")), i18nc("@title:tab General View settings", "General"));
+    connect(generalViewPage, &GeneralViewSettingsPage::changed, this, &ViewSettingsPage::changed);
 
     // Content Display Tab
     contentDisplayTab = new ContentDisplayTab(tabWidget);

src/settings/viewmodes/viewsettingspage.h
@@ -24,7 +24,7 @@ class ViewSettingsPage : public SettingsPageBase
     Q_OBJECT
 
 public:
-    explicit ViewSettingsPage(QWidget *parent);
+    explicit ViewSettingsPage(const QUrl &url, QWidget *parent);
     ~ViewSettingsPage() override;
 
     /** @see SettingsPageBase::applySettings() */

src/settings/dolphinsettingsdialog.cpp
@@ -10,9 +10,7 @@
 #include "contextmenu/contextmenusettingspage.h"
 #include "dolphin_generalsettings.h"
 #include "dolphinmainwindow.h"
-#include "general/generalsettingspage.h"
-#include "navigation/navigationsettingspage.h"
-#include "startup/startupsettingspage.h"
+#include "interface/interfacesettingspage.h"
 #include "trash/trashsettingspage.h"
 #include "viewmodes/viewsettingspage.h"
 #if HAVE_KUSERFEEDBACK
@@ -50,29 +48,17 @@ DolphinSettingsDialog::DolphinSettingsDialog(const QUrl &url, QWidget *parent, K
     connect(box->button(QDialogButtonBox::Apply), &QAbstractButton::clicked, this, &DolphinSettingsDialog::applySettings);
     connect(box->button(QDialogButtonBox::RestoreDefaults), &QAbstractButton::clicked, this, &DolphinSettingsDialog::restoreDefaults);
 
-    // General
-    GeneralSettingsPage *generalSettingsPage = new GeneralSettingsPage(url, this);
-    KPageWidgetItem *generalSettingsFrame = addPage(generalSettingsPage, i18nc("@title:group General settings", "General"));
-    generalSettingsFrame->setIcon(QIcon::fromTheme(QStringLiteral("system-file-manager")));
-    connect(generalSettingsPage, &GeneralSettingsPage::changed, this, &DolphinSettingsDialog::enableApply);
+    // Interface
+    InterfaceSettingsPage *interfaceSettingsPage = new InterfaceSettingsPage(this);
+    KPageWidgetItem *interfaceSettingsFrame = addPage(interfaceSettingsPage, i18nc("@title:group Interface settings", "Interface"));
+    interfaceSettingsFrame->setIcon(QIcon::fromTheme(QStringLiteral("system-file-manager")));
+    connect(interfaceSettingsPage, &InterfaceSettingsPage::changed, this, &DolphinSettingsDialog::enableApply);
 
-    // Startup
-    StartupSettingsPage *startupSettingsPage = new StartupSettingsPage(url, this);
-    KPageWidgetItem *startupSettingsFrame = addPage(startupSettingsPage, i18nc("@title:group", "Startup"));
-    startupSettingsFrame->setIcon(QIcon::fromTheme(QStringLiteral("preferences-desktop-launch-feedback")));
-    connect(startupSettingsPage, &StartupSettingsPage::changed, this, &DolphinSettingsDialog::enableApply);
-
-    // View Modes
-    ViewSettingsPage *viewSettingsPage = new ViewSettingsPage(this);
-    KPageWidgetItem *viewSettingsFrame = addPage(viewSettingsPage, i18nc("@title:group", "View Modes"));
+    // View
+    ViewSettingsPage *viewSettingsPage = new ViewSettingsPage(url, this);
+    KPageWidgetItem *viewSettingsFrame = addPage(viewSettingsPage, i18nc("@title:group", "View"));
     viewSettingsFrame->setIcon(QIcon::fromTheme(QStringLiteral("preferences-desktop-icons")));
     connect(viewSettingsPage, &ViewSettingsPage::changed, this, &DolphinSettingsDialog::enableApply);
-
-    // Navigation
-    NavigationSettingsPage *navigationSettingsPage = new NavigationSettingsPage(this);
-    KPageWidgetItem *navigationSettingsFrame = addPage(navigationSettingsPage, i18nc("@title:group", "Navigation"));
-    navigationSettingsFrame->setIcon(QIcon::fromTheme(QStringLiteral("preferences-desktop-navigation")));
-    connect(navigationSettingsPage, &NavigationSettingsPage::changed, this, &DolphinSettingsDialog::enableApply);
 
     // Context Menu
     auto contextMenuSettingsPage = new ContextMenuSettingsPage(this,
@@ -113,10 +99,8 @@ DolphinSettingsDialog::DolphinSettingsDialog(const QUrl &url, QWidget *parent, K
     }
 #endif
 
-    m_pages.append(generalSettingsPage);
-    m_pages.append(startupSettingsPage);
+    m_pages.append(interfaceSettingsPage);
     m_pages.append(viewSettingsPage);
-    m_pages.append(navigationSettingsPage);
     m_pages.append(contextMenuSettingsPage);
     if (trashSettingsPage) {
         m_pages.append(trashSettingsPage);

src/settings/general/behaviorsettingspage.cpp
@@ -1,195 +1,0 @@
-/*
- * SPDX-FileCopyrightText: 2006 Peter Penz (peter.penz@gmx.at) and Patrice Tremblay
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
-#include "behaviorsettingspage.h"
-
-#include "global.h"
-#include "views/viewproperties.h"
-
-#include <KLocalizedString>
-
-#include <QButtonGroup>
-#include <QCheckBox>
-#include <QFormLayout>
-#include <QRadioButton>
-#include <QSpacerItem>
-
-BehaviorSettingsPage::BehaviorSettingsPage(const QUrl &url, QWidget *parent)
-    : SettingsPageBase(parent)
-    , m_url(url)
-    , m_localViewProps(nullptr)
-    , m_globalViewProps(nullptr)
-    , m_showToolTips(nullptr)
-    , m_showSelectionToggle(nullptr)
-    , m_naturalSorting(nullptr)
-    , m_caseSensitiveSorting(nullptr)
-    , m_caseInsensitiveSorting(nullptr)
-    , m_renameInline(nullptr)
-    , m_useTabForSplitViewSwitch(nullptr)
-{
-    QFormLayout *topLayout = new QFormLayout(this);
-
-    // View properties
-    m_globalViewProps = new QRadioButton(i18nc("@option:radio", "Use common display style for all folders"));
-    m_localViewProps = new QRadioButton(i18nc("@option:radio", "Remember display style for each folder"));
-    m_localViewProps->setToolTip(i18nc("@info", "Dolphin will create a hidden .directory file in each folder you change view properties for."));
-
-    QButtonGroup *viewGroup = new QButtonGroup(this);
-    viewGroup->addButton(m_globalViewProps);
-    viewGroup->addButton(m_localViewProps);
-    topLayout->addRow(i18nc("@title:group", "View: "), m_globalViewProps);
-    topLayout->addRow(QString(), m_localViewProps);
-
-    topLayout->addItem(new QSpacerItem(0, Dolphin::VERTICAL_SPACER_HEIGHT, QSizePolicy::Fixed, QSizePolicy::Fixed));
-
-    // Sorting properties
-    m_naturalSorting = new QRadioButton(i18nc("option:radio", "Natural"));
-    m_caseInsensitiveSorting = new QRadioButton(i18nc("option:radio", "Alphabetical, case insensitive"));
-    m_caseSensitiveSorting = new QRadioButton(i18nc("option:radio", "Alphabetical, case sensitive"));
-
-    QButtonGroup *sortingModeGroup = new QButtonGroup(this);
-    sortingModeGroup->addButton(m_naturalSorting);
-    sortingModeGroup->addButton(m_caseInsensitiveSorting);
-    sortingModeGroup->addButton(m_caseSensitiveSorting);
-    topLayout->addRow(i18nc("@title:group", "Sorting mode: "), m_naturalSorting);
-    topLayout->addRow(QString(), m_caseInsensitiveSorting);
-    topLayout->addRow(QString(), m_caseSensitiveSorting);
-
-    // Split Views
-    topLayout->addItem(new QSpacerItem(0, Dolphin::VERTICAL_SPACER_HEIGHT, QSizePolicy::Fixed, QSizePolicy::Fixed));
-
-    // 'Switch between panes of split views with tab key'
-    m_useTabForSplitViewSwitch = new QCheckBox(i18nc("option:check split view panes", "Switch between panes with Tab key"));
-    topLayout->addRow(i18nc("@title:group", "Split view: "), m_useTabForSplitViewSwitch);
-
-    // 'Close active pane when turning off split view'
-    m_closeActiveSplitView = new QCheckBox(i18nc("option:check", "Turning off split view closes active pane"));
-    topLayout->addRow(QString(), m_closeActiveSplitView);
-    m_closeActiveSplitView->setToolTip(i18n("When deactivated, turning off split view will close the inactive pane"));
-
-    topLayout->addItem(new QSpacerItem(0, Dolphin::VERTICAL_SPACER_HEIGHT, QSizePolicy::Fixed, QSizePolicy::Fixed));
-
-#if HAVE_BALOO
-    // 'Show tooltips'
-    m_showToolTips = new QCheckBox(i18nc("@option:check", "Show tooltips"));
-    topLayout->addRow(i18nc("@title:group", "Miscellaneous: "), m_showToolTips);
-#endif
-
-    // 'Show selection marker'
-    m_showSelectionToggle = new QCheckBox(i18nc("@option:check", "Show selection marker"));
-#if HAVE_BALOO
-    topLayout->addRow(QString(), m_showSelectionToggle);
-#else
-    topLayout->addRow(i18nc("@title:group", "Miscellaneous: "), m_showSelectionToggle);
-#endif
-
-    // 'Inline renaming of items'
-    m_renameInline = new QCheckBox(i18nc("option:check", "Rename inline"));
-    topLayout->addRow(QString(), m_renameInline);
-
-    loadSettings();
-
-    connect(m_localViewProps, &QRadioButton::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_globalViewProps, &QRadioButton::toggled, this, &BehaviorSettingsPage::changed);
-#if HAVE_BALOO
-    connect(m_showToolTips, &QCheckBox::toggled, this, &BehaviorSettingsPage::changed);
-#endif
-    connect(m_showSelectionToggle, &QCheckBox::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_naturalSorting, &QRadioButton::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_caseInsensitiveSorting, &QRadioButton::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_caseSensitiveSorting, &QRadioButton::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_renameInline, &QCheckBox::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_useTabForSplitViewSwitch, &QCheckBox::toggled, this, &BehaviorSettingsPage::changed);
-    connect(m_closeActiveSplitView, &QCheckBox::toggled, this, &BehaviorSettingsPage::changed);
-}
-
-BehaviorSettingsPage::~BehaviorSettingsPage()
-{
-}
-
-void BehaviorSettingsPage::applySettings()
-{
-    GeneralSettings *settings = GeneralSettings::self();
-    ViewProperties props(m_url); // read current view properties
-
-    const bool useGlobalViewProps = m_globalViewProps->isChecked();
-    settings->setGlobalViewProps(useGlobalViewProps);
-#if HAVE_BALOO
-    settings->setShowToolTips(m_showToolTips->isChecked());
-#endif
-    settings->setShowSelectionToggle(m_showSelectionToggle->isChecked());
-    setSortingChoiceValue(settings);
-    settings->setRenameInline(m_renameInline->isChecked());
-    settings->setUseTabForSwitchingSplitView(m_useTabForSplitViewSwitch->isChecked());
-    settings->setCloseActiveSplitView(m_closeActiveSplitView->isChecked());
-    settings->save();
-
-    if (useGlobalViewProps) {
-        // Remember the global view properties by applying the current view properties.
-        // It is important that GeneralSettings::globalViewProps() is set before
-        // the class ViewProperties is used, as ViewProperties uses this setting
-        // to find the destination folder for storing the view properties.
-        ViewProperties globalProps(m_url);
-        globalProps.setDirProperties(props);
-    }
-}
-
-void BehaviorSettingsPage::restoreDefaults()
-{
-    GeneralSettings *settings = GeneralSettings::self();
-    settings->useDefaults(true);
-    loadSettings();
-    settings->useDefaults(false);
-}
-
-void BehaviorSettingsPage::loadSettings()
-{
-    const bool useGlobalViewProps = GeneralSettings::globalViewProps();
-    m_localViewProps->setChecked(!useGlobalViewProps);
-    m_globalViewProps->setChecked(useGlobalViewProps);
-
-#if HAVE_BALOO
-    m_showToolTips->setChecked(GeneralSettings::showToolTips());
-#endif
-    m_showSelectionToggle->setChecked(GeneralSettings::showSelectionToggle());
-    m_renameInline->setChecked(GeneralSettings::renameInline());
-    m_useTabForSplitViewSwitch->setChecked(GeneralSettings::useTabForSwitchingSplitView());
-    m_closeActiveSplitView->setChecked(GeneralSettings::closeActiveSplitView());
-
-    loadSortingChoiceSettings();
-}
-
-void BehaviorSettingsPage::setSortingChoiceValue(GeneralSettings *settings)
-{
-    using Choice = GeneralSettings::EnumSortingChoice;
-    if (m_naturalSorting->isChecked()) {
-        settings->setSortingChoice(Choice::NaturalSorting);
-    } else if (m_caseInsensitiveSorting->isChecked()) {
-        settings->setSortingChoice(Choice::CaseInsensitiveSorting);
-    } else if (m_caseSensitiveSorting->isChecked()) {
-        settings->setSortingChoice(Choice::CaseSensitiveSorting);
-    }
-}
-
-void BehaviorSettingsPage::loadSortingChoiceSettings()
-{
-    using Choice = GeneralSettings::EnumSortingChoice;
-    switch (GeneralSettings::sortingChoice()) {
-    case Choice::NaturalSorting:
-        m_naturalSorting->setChecked(true);
-        break;
-    case Choice::CaseInsensitiveSorting:
-        m_caseInsensitiveSorting->setChecked(true);
-        break;
-    case Choice::CaseSensitiveSorting:
-        m_caseSensitiveSorting->setChecked(true);
-        break;
-    default:
-        Q_UNREACHABLE();
-    }
-}
-
-#include "moc_behaviorsettingspage.cpp"

src/settings/general/generalsettingspage.cpp
@@ -1,74 +1,0 @@
-/*
- * SPDX-FileCopyrightText: 2006 Peter Penz <peter.penz@gmx.at>
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
-#include "generalsettingspage.h"
-
-#include "behaviorsettingspage.h"
-#include "confirmationssettingspage.h"
-#include "previewssettingspage.h"
-#include "statusbarsettingspage.h"
-
-#include <KLocalizedString>
-
-#include <QTabWidget>
-#include <QVBoxLayout>
-
-GeneralSettingsPage::GeneralSettingsPage(const QUrl &url, QWidget *parent)
-    : SettingsPageBase(parent)
-    , m_pages()
-{
-    QVBoxLayout *topLayout = new QVBoxLayout(this);
-    topLayout->setContentsMargins(0, 0, 0, 0);
-
-    QTabWidget *tabWidget = new QTabWidget(this);
-
-    // initialize 'Behavior' tab
-    BehaviorSettingsPage *behaviorPage = new BehaviorSettingsPage(url, tabWidget);
-    tabWidget->addTab(behaviorPage, i18nc("@title:tab Behavior settings", "Behavior"));
-    connect(behaviorPage, &BehaviorSettingsPage::changed, this, &GeneralSettingsPage::changed);
-
-    // initialize 'Previews' tab
-    PreviewsSettingsPage *previewsPage = new PreviewsSettingsPage(tabWidget);
-    tabWidget->addTab(previewsPage, i18nc("@title:tab Previews settings", "Previews"));
-    connect(previewsPage, &PreviewsSettingsPage::changed, this, &GeneralSettingsPage::changed);
-
-    // initialize 'Context Menu' tab
-    ConfirmationsSettingsPage *confirmationsPage = new ConfirmationsSettingsPage(tabWidget);
-    tabWidget->addTab(confirmationsPage, i18nc("@title:tab Confirmations settings", "Confirmations"));
-    connect(confirmationsPage, &ConfirmationsSettingsPage::changed, this, &GeneralSettingsPage::changed);
-
-    // initialize 'Status Bar' tab
-    StatusBarSettingsPage *statusBarPage = new StatusBarSettingsPage(tabWidget);
-    tabWidget->addTab(statusBarPage, i18nc("@title:tab Status Bar settings", "Status Bar"));
-    connect(statusBarPage, &StatusBarSettingsPage::changed, this, &GeneralSettingsPage::changed);
-
-    m_pages.append(behaviorPage);
-    m_pages.append(previewsPage);
-    m_pages.append(confirmationsPage);
-    m_pages.append(statusBarPage);
-
-    topLayout->addWidget(tabWidget, 0, {});
-}
-
-GeneralSettingsPage::~GeneralSettingsPage()
-{
-}
-
-void GeneralSettingsPage::applySettings()
-{
-    for (SettingsPageBase *page : qAsConst(m_pages)) {
-        page->applySettings();
-    }
-}
-
-void GeneralSettingsPage::restoreDefaults()
-{
-    for (SettingsPageBase *page : qAsConst(m_pages)) {
-        page->restoreDefaults();
-    }
-}
-
-#include "moc_generalsettingspage.cpp"

src/settings/general/statusbarsettingspage.cpp
@@ -1,77 +1,0 @@
-/*
- * SPDX-FileCopyrightText: 2009 Peter Penz <peter.penz19@gmail.com>
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
-#include "statusbarsettingspage.h"
-
-#include "dolphin_generalsettings.h"
-
-#include <KLocalizedString>
-
-#include <QCheckBox>
-#include <QVBoxLayout>
-
-StatusBarSettingsPage::StatusBarSettingsPage(QWidget *parent)
-    : SettingsPageBase(parent)
-    , m_showStatusBar(nullptr)
-    , m_showZoomSlider(nullptr)
-    , m_showSpaceInfo(nullptr)
-{
-    m_showStatusBar = new QCheckBox(i18nc("@option:check", "Show status bar"), this);
-    m_showZoomSlider = new QCheckBox(i18nc("@option:check", "Show zoom slider"), this);
-    m_showSpaceInfo = new QCheckBox(i18nc("@option:check", "Show space information"), this);
-
-    QVBoxLayout *topLayout = new QVBoxLayout(this);
-    topLayout->addWidget(m_showStatusBar);
-    topLayout->addWidget(m_showZoomSlider);
-    topLayout->addWidget(m_showSpaceInfo);
-    topLayout->addStretch();
-
-    loadSettings();
-
-    connect(m_showStatusBar, &QCheckBox::toggled, this, &StatusBarSettingsPage::changed);
-    connect(m_showStatusBar, &QCheckBox::toggled, this, &StatusBarSettingsPage::onShowStatusBarToggled);
-    connect(m_showZoomSlider, &QCheckBox::toggled, this, &StatusBarSettingsPage::changed);
-    connect(m_showSpaceInfo, &QCheckBox::toggled, this, &StatusBarSettingsPage::changed);
-}
-
-StatusBarSettingsPage::~StatusBarSettingsPage()
-{
-}
-
-void StatusBarSettingsPage::onShowStatusBarToggled()
-{
-    const bool checked = m_showStatusBar->isChecked();
-    m_showZoomSlider->setEnabled(checked);
-    m_showSpaceInfo->setEnabled(checked);
-}
-
-void StatusBarSettingsPage::applySettings()
-{
-    GeneralSettings *settings = GeneralSettings::self();
-    settings->setShowStatusBar(m_showStatusBar->isChecked());
-    settings->setShowZoomSlider(m_showZoomSlider->isChecked());
-    settings->setShowSpaceInfo(m_showSpaceInfo->isChecked());
-    settings->save();
-}
-
-void StatusBarSettingsPage::restoreDefaults()
-{
-    GeneralSettings *settings = GeneralSettings::self();
-    settings->useDefaults(true);
-    loadSettings();
-    settings->useDefaults(false);
-}
-
-void StatusBarSettingsPage::loadSettings()
-{
-    m_showStatusBar->setChecked(GeneralSettings::showStatusBar());
-    m_showZoomSlider->setChecked(GeneralSettings::showZoomSlider());
-    m_showSpaceInfo->setChecked(GeneralSettings::showSpaceInfo());
-
-    onShowStatusBarToggled();
-}
-
-#include "moc_statusbarsettingspage.cpp"

commit ff852b97f110691cda4c2f63f8d0f6bf8759fe2f

Re-Allow to set settings in Settings-> View-> General page
Regressed in 489b56b68bb29e81337e115c490eea4403001b71

Simplify implementation by sharing making class ViewSettingsPage implement SettingsPageBase sharing implementation with other tabs in viewsettingspage

src/CMakeLists.txt
@@ -505,6 +505,7 @@ if(NOT WIN32)
         settings/viewmodes/dolphinfontrequester.cpp
         settings/viewmodes/viewmodesettings.cpp
         settings/viewmodes/viewsettingstab.cpp
+        settings/settingspagebase.cpp
         views/zoomlevelinfo.cpp
         settings/kcm/kcmdolphinviewmodes.h
         settings/viewmodes/dolphinfontrequester.h

src/settings/kcm/kcmdolphinviewmodes.cpp
@@ -67,7 +67,7 @@ void DolphinViewModesConfigModule::save()
 void DolphinViewModesConfigModule::defaults()
 {
     for (ViewSettingsTab *tab : qAsConst(m_tabs)) {
-        tab->restoreDefaultSettings();
+        tab->restoreDefaults();
     }
     reparseConfiguration();
 }

src/settings/viewmodes/viewsettingspage.cpp
@@ -50,6 +50,8 @@ ViewSettingsPage::ViewSettingsPage(const QUrl &url, QWidget *parent)
     tabWidget->addTab(detailsTab, QIcon::fromTheme(QStringLiteral("view-list-tree")), i18nc("@title:tab", "Details"));
     connect(detailsTab, &ViewSettingsTab::changed, this, &ViewSettingsPage::changed);
 
+    m_tabs.append(generalViewPage);
+    m_tabs.append(contentDisplayTab);
     m_tabs.append(iconsTab);
     m_tabs.append(compactTab);
     m_tabs.append(detailsTab);
@@ -63,23 +65,16 @@ ViewSettingsPage::~ViewSettingsPage()
 
 void ViewSettingsPage::applySettings()
 {
-    contentDisplayTab->applySettings();
-
-    for (ViewSettingsTab *tab : qAsConst(m_tabs)) {
+    for (SettingsPageBase *tab : qAsConst(m_tabs)) {
         tab->applySettings();
     }
 }
 
 void ViewSettingsPage::restoreDefaults()
 {
-    if (tabWidget->currentWidget() == contentDisplayTab) {
-        contentDisplayTab->restoreDefaults();
-        return;
-    }
-
-    for (ViewSettingsTab *tab : qAsConst(m_tabs)) {
+    for (SettingsPageBase *tab : qAsConst(m_tabs)) {
         if (tabWidget->currentWidget() == tab) {
-            tab->restoreDefaultSettings();
+            tab->restoreDefaults();
             return;
         }
     }

src/settings/viewmodes/viewsettingspage.h
@@ -36,7 +36,7 @@ public:
 private:
     ContentDisplayTab *contentDisplayTab;
     QTabWidget *tabWidget;
-    QList<ViewSettingsTab *> m_tabs;
+    QList<SettingsPageBase *> m_tabs;
 };
 
 #endif

src/settings/viewmodes/viewsettingstab.cpp
@@ -26,7 +26,7 @@
 #include <QSpinBox>
 
 ViewSettingsTab::ViewSettingsTab(Mode mode, QWidget *parent)
-    : QWidget(parent)
+    : SettingsPageBase(parent)
     , m_mode(mode)
     , m_defaultSizeSlider(nullptr)
     , m_previewSizeSlider(nullptr)
@@ -186,7 +186,7 @@ void ViewSettingsTab::applySettings()
     settings.save();
 }
 
-void ViewSettingsTab::restoreDefaultSettings()
+void ViewSettingsTab::restoreDefaults()
 {
     ViewModeSettings settings(m_mode);
     settings.useDefaults(true);

src/settings/viewmodes/viewsettingstab.h
@@ -7,7 +7,7 @@
 #ifndef VIEWSETTINGSTAB_H
 #define VIEWSETTINGSTAB_H
 
-#include <QWidget>
+#include "settings/settingspagebase.h"
 
 class DolphinFontRequester;
 class QComboBox;
@@ -19,7 +19,7 @@ class QRadioButton;
 /**
  * @brief Represents one tab of the view-settings page.
  */
-class ViewSettingsTab : public QWidget
+class ViewSettingsTab : public SettingsPageBase
 {
     Q_OBJECT
 
@@ -29,8 +29,8 @@ public:
     explicit ViewSettingsTab(Mode mode, QWidget *parent = nullptr);
     ~ViewSettingsTab() override;
 
-    void applySettings();
-    void restoreDefaultSettings();
+    void applySettings() override;
+    void restoreDefaults() override;
 
 Q_SIGNALS:
     void changed();