50268e80244b0a35eda7027ffece9e2b0326dc8c
l10n daemon script <scripty@kde.org>
2023-11-07 03:28:53 +0000

    GIT_SILENT Sync po/docbooks with svn

--- po/fi/scripts/dolphin/dolphin.js
+++
@@ -1,7 +0,0 @@
-function fixDateFormatInDolphinGroupTitle(phr) {
-    phr = Ts.toUpperFirst(phr);
-    phr = phr.replace("kuuta", "kuussa");
-    return phr;
-}
-
-Ts.setcall("korjaa_päiväysmuotoilu_ryhmän_nimessä", fixDateFormatInDolphinGroupTitle);
