c8b3d90a576713fd16b3045dcd5d7d8bc25a1a49
Konstantin Kharlamov <Hi-Angel@yandex.ru>
2024-02-29 20:56:53 +0300

    dolphin.zsh: complete both directories and URL protocols
    Dolphin is mostly used for navigating local paths (or remote paths
    mounted locally), so it is important to have directories completed as
    well as URLs

--- completions/dolphin.zsh
+++ completions/dolphin.zsh
@@ -6,13 +6,19 @@
 
 local ret=1
 
+# complete both URL protocols and local directories
+_urls_and_dirs() {
+  _urls
+  _directories
+}
+
 _arguments -C \
   '(* -)'{-h,--help}'[Displays help on commandline options]' \
   '--select[The files and folders passed as arguments will be selected.]' \
   '--split[Dolphin will get started with a split view.]' \
   '--new-window[Dolphin will explicitly open in a new window.]' \
   '--daemon[Start Dolphin Daemon (only required for DBus Interface).]' \
-  '*:: :_urls' \
+  '*:: :_urls_and_dirs' \
   && ret=0
 
 return $ret
