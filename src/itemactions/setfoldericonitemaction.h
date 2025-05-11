4d2dc0961d11eed493e7f5103fd12735d456c1d7
Méven Car <meven@kde.org>
2025-05-11 13:47:36 +0200

    Add a SetFolderIcon ItemAction plugin
    To allow to change folder icon from the context menu.
    
    CCBUG: 467221

---
+++ src/itemactions/setfoldericonitemaction.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Méven Car <meven@kde.org>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#include <KAbstractFileItemActionPlugin>
+#include <KFileItemListProperties>
+
+class SetFolderIconItemAction : public KAbstractFileItemActionPlugin
+{
+    Q_OBJECT
+
+public:
+    SetFolderIconItemAction(QObject *parent);
+
+    QList<QAction *> actions(const KFileItemListProperties &fileItemInfos, QWidget *parentWidget) override;
+
+private:
+    void setFolderIcon(bool check);
+    QUrl m_localUrl;
+};
