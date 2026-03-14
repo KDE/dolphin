52295defce9db63b0d5088037b204656675592b9
Méven Car <meven@kde.org>
2026-03-14 13:06:47 +0100

    dolphincolumnsview: Don't change column focus on selection changes

--- src/views/dolphincolumnsview.cpp
+++ src/views/dolphincolumnsview.cpp
@@ -281,6 +281,8 @@ void DolphinColumnsView::slotColumnsCurrentItemChanged(const KFileItem &item)
         return;
     }
 
+    senderPane->controller()->selectionManager()->blockSignals(true);
+
     // Directory → open child column (no auto-select in the child)
     const QUrl folderUrl = DolphinView::openItemAsFolderUrl(item, false);
     if (!folderUrl.isEmpty()) {
@@ -301,6 +303,8 @@ void DolphinColumnsView::slotColumnsCurrentItemChanged(const KFileItem &item)
 
         Q_EMIT urlChanged(m_columns.at(colIndex)->dirUrl());
     }
+
+    senderPane->controller()->selectionManager()->blockSignals(false);
 }
 
 void DolphinColumnsView::slotPaneLoadingCompleted()
@@ -506,7 +510,10 @@ bool DolphinColumnsView::eventFilter(QObject *watched, QEvent *event)
     }
 
     if (sourceColumn < 0) {
-        return DolphinView::eventFilter(watched, event);
+        m_blockNavigation = true;
+        auto ret = DolphinView::eventFilter(watched, event);
+        m_blockNavigation = false;
+        return ret;
     }
 
     if (event->type() == QEvent::KeyPress) {
@@ -594,8 +601,7 @@ void DolphinColumnsView::handleKeyRight(int sourceColumn)
             m_pendingAutoSelect = m_columns.at(childCol);
         }
         setActiveColumn(childCol);
-        updateUrl(m_columns.at(m_activeColumn)->dirUrl());
-        Q_EMIT urlChanged(url());
+        Q_EMIT urlChanged(m_columns.at(childCol)->dirUrl());
     }
 }
 
@@ -630,9 +636,8 @@ bool DolphinColumnsView::handleKeyReturn(int sourceColumn)
         } else {
             m_pendingAutoSelect = m_columns.at(childCol);
         }
-        setActiveColumn(childCol);
-        updateUrl(m_columns.at(m_activeColumn)->dirUrl());
-        Q_EMIT urlChanged(url());
+        updateUrl(m_columns.at(childCol)->dirUrl());
+        Q_EMIT urlChanged(m_columns.at(childCol)->dirUrl());
     }
     return true;
 }
@@ -896,17 +901,22 @@ void DolphinColumnsView::reconnectActivePane(DolphinColumnPane *oldPane, Dolphin
         }
 
         auto currentColumnIndex = m_columns.indexOf(newPane);
+        auto currentUrl = newPane->dirUrl();
         auto item = newPane->model()->fileItem(current.first());
-        if (item.isNull() || !item.isDir()) {
-            popAfter(currentColumnIndex);
-            return;
+        if (!item.isNull() && item.isDir()) {
+            for (int i = m_columns.size() - 1; i > 0; --i) {
+                if (currentUrl.isParentOf(m_columns.at(i)->dirUrl())) {
+                    popAfter(i - 1);
+                } else {
+                    break;
+                }
+            }
         }
 
-        if (currentColumnIndex != -1) {
+        if (item.isDir() && currentColumnIndex != -1) {
             newPane->controller()->selectionManager()->blockSignals(true);
             openChild(currentColumnIndex, item.url());
 
-            setActiveColumn(currentColumnIndex + 1);
             updateUrl(m_columns.at(m_activeColumn)->dirUrl());
             Q_EMIT urlChanged(url());
 
