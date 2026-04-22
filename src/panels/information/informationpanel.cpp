KDE/dolphin

commit bad63660b4c3253fdbd5fc3f989146519a7338b7

informationpanel: ensure selection info is shown when using keyboard navigation
The original implementation prioritized mouse hover over keyboard selection,
preventing panel updates when navigating with arrow keys while the mouse
was over another item.

This patch adds explicit state tracking to ensure keyboard selections
always update the panel, regardless of mouse position.

BUG: 474507

src/panels/information/informationpanel.cpp
@@ -60,11 +60,13 @@ void InformationPanel::setSelection(const KFileItemList &selection)
             m_shownUrl = url();
             showItemInfo();
         }
+        m_isSelectionActive = false;
         m_infoTimer->stop();
     } else {
         if ((count == 1) && !selection.first().url().isEmpty()) {
             m_urlCandidate = selection.first().url();
         }
+        m_isSelectionActive = true;
         showItemInfo();
     }
 }
@@ -87,6 +89,8 @@ void InformationPanel::requestDelayedItemInfo(const KFileItem &item)
     }
 
     cancelRequest();
+
+    m_isSelectionActive = false;
 
     m_hoveredItem = item;
     m_infoTimer->start();
@@ -231,9 +235,10 @@ void InformationPanel::showItemInfo()
     } else {
         // The information for exactly one item should be shown
         KFileItem item;
-        if (!m_hoveredItem.isNull() && InformationPanelSettings::showHovered()) {
+
+        if (!m_isSelectionActive && !m_hoveredItem.isNull() && InformationPanelSettings::showHovered()) {
             item = m_hoveredItem;
-        } else if (!m_selection.isEmpty()) {
+        } else if (m_isSelectionActive && !m_selection.isEmpty()) {
             Q_ASSERT(m_selection.count() == 1);
             item = m_selection.first();
         }

src/panels/information/informationpanel.h
@@ -147,6 +147,8 @@ private:
     KFileItem m_hoveredItem;
     KFileItemList m_selection;
 
+    bool m_isSelectionActive = false;
+
     KIO::Job *m_folderStatJob;
 
     InformationPanelContent *m_content;

commit 35f2bb94a5c0ec7d5d5e262356876a782a4d1ea9

panels/information: when not hovering anything show selection
This regressed after bad63660b4c3253fdbd5fc3f989146519a7338b7.
This also could cause crashes when asserts are active.

src/panels/information/informationpanel.cpp
@@ -84,14 +84,12 @@ void InformationPanel::requestDelayedItemInfo(const KFileItem &item)
         return;
     }
 
-    if (item.isNull()) {
-        m_hoveredItem = KFileItem();
-        return;
-    }
-
     cancelRequest();
-
-    m_isSelectionActive = false;
+    if (!item.isNull()) {
+        m_isSelectionActive = false;
+    } else {
+        m_isSelectionActive = !m_selection.isEmpty();
+    }
 
     m_hoveredItem = item;
     m_infoTimer->start();
@@ -230,35 +228,36 @@ void InformationPanel::showItemInfo()
     cancelRequest();
     //qDebug() << "showItemInfo" << m_fileItem;
 
-    if (m_hoveredItem.isNull() && (m_selection.count() > 1)) {
+    bool canShowHoverItem = !m_isSelectionActive && !m_hoveredItem.isNull() && InformationPanelSettings::showHovered();
+    if (m_selection.count() > 1 && !canShowHoverItem) {
         // The information for a selection of items should be shown
         m_content->showItems(m_selection);
-    } else {
-        // The information for exactly one item should be shown
-        KFileItem item;
-
-        if (!m_isSelectionActive && !m_hoveredItem.isNull() && InformationPanelSettings::showHovered()) {
-            item = m_hoveredItem;
-        } else if (m_isSelectionActive && !m_selection.isEmpty()) {
-            Q_ASSERT(m_selection.count() == 1);
-            item = m_selection.first();
-        }
-
-        if (!item.isNull()) {
-            m_shownUrl = item.url();
-            m_content->showItem(item);
-            return;
-        }
-
-        // No item is hovered and no selection has been done: provide
-        // an item for the currently shown directory.
-        m_shownUrl = url();
-        m_folderStatJob = KIO::stat(m_shownUrl, KIO::StatJob::SourceSide, KIO::StatDefaultDetails | KIO::StatRecursiveSize, KIO::HideProgressInfo);
-        if (m_folderStatJob->uiDelegate()) {
-            KJobWidgets::setWindow(m_folderStatJob, this);
-        }
-        connect(m_folderStatJob, &KIO::Job::result, this, &InformationPanel::slotFolderStatFinished);
+        return;
     }
+
+    // The information for exactly one item should be shown
+    KFileItem item;
+    if (canShowHoverItem) {
+        item = m_hoveredItem;
+    } else if (!m_selection.isEmpty()) {
+        Q_ASSERT(m_selection.count() == 1);
+        item = m_selection.first();
+    }
+
+    if (!item.isNull()) {
+        m_shownUrl = item.url();
+        m_content->showItem(item);
+        return;
+    }
+
+    // No item is hovered and no selection has been done: provide
+    // an item for the currently shown directory.
+    m_shownUrl = url();
+    m_folderStatJob = KIO::stat(m_shownUrl, KIO::StatJob::SourceSide, KIO::StatDefaultDetails | KIO::StatRecursiveSize, KIO::HideProgressInfo);
+    if (m_folderStatJob->uiDelegate()) {
+        KJobWidgets::setWindow(m_folderStatJob, this);
+    }
+    connect(m_folderStatJob, &KIO::Job::result, this, &InformationPanel::slotFolderStatFinished);
 }
 
 void InformationPanel::slotFolderStatFinished(KJob *job)