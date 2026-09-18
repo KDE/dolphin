73781e43431348490b99ee6cc3f265f531808102
Méven Car <meven@kde.org>
2026-09-18 05:52:14 +0200

    about: update copyright year

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ int main(int argc, char **argv)
                          QStringLiteral(DOLPHIN_VERSION_STRING),
                          i18nc("@title", "File Manager"),
                          KAboutLicense::GPL,
-                         i18nc("@info:credit", "© 2006–2025 The Dolphin Developers"));
+                         i18nc("@info:credit", "© 2006–2026 The Dolphin Developers"));
     aboutData.setHomepage(QStringLiteral("https://apps.kde.org/dolphin"));
     aboutData.addAuthor(i18nc("@info:credit", "Felix Ernst"),
                         i18nc("@info:credit", "Maintainer (since 2021) and developer"),
