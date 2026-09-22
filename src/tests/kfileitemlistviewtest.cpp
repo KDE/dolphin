f421a27c76423a14b088190f5d7ef0df19926d11
Mohanraj Velayutham <mohanrajvelayutham08@gmail.com>
2026-09-22 16:07:21 +0530

    test: clean up comment in testInstantAnimationDuration

--- src/tests/kfileitemlistviewtest.cpp
+++ src/tests/kfileitemlistviewtest.cpp
@@ -143,7 +143,6 @@ void KFileItemListViewTest::testInstantAnimationDuration()
 
     // With instant animation (SH_Widget_Animation_Duration == 0), the animation duration is 1ms
     // and completes in the next event loop tick.
-    // this wait(100) fails because the animation takes 200ms.
     QVERIFY(finishedSpy.wait(100));
     QVERIFY(!anim.isStarted(&widget, KItemListViewAnimation::DeleteAnimation));
 }
